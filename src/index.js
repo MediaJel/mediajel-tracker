16d186224c060ae926533114ac10a677e3ff51d4
Pacholo Amit <pacholoamit.tech@gmail.com>
2021-04-30 11:38:31 +0800

    Refactoring of index.js to es6

--- src/index.js
+++ src/index.js
@@ -6,37 +6,28 @@ let mediajelAppId = null;
 let environment = null;
 let production = true;
 
-//Loops over all scripts & collects scripts with arguments.
-for (let i = 0, len = scripts.length; i < len; i++) {
-  let src = scripts[i].getAttribute('src');
-  //Checks for scripts containing mediajelAppId
-  if (src.includes('mediajelAppId')) {
-    let srcArg = src.split('?');
-    let args = srcArg[1];
-    let argv = args.split('&');
-    //Loops over all results & collects value of argument "mediajelAppId"
-    for (let j = 0, argc = argv.length; j < argc; j++) {
-      let pair = argv[j].split('=');
-      let argName = pair[0];
-      let argValue = pair[1];
+const scriptArgs = Array.from(scripts).map((script) => {
+  let src = script.getAttribute('src');
+  let srcArg = src.split('?');
+  let args = srcArg[1];
+  return args.split('&');
+});
 
-      switch (argName) {
-        case 'mediajelAppId':
-          mediajelAppId = argValue;
-          break;
-        case 'environment':
-          environment = argValue.toLowerCase();
-          break;
-        case 'test':
-          production = false;
-      }
-    }
+scriptArgs[0].map((arg) => {
+  let pair = arg.split('=');
+  let argName = pair[0];
+  let argValue = pair[1];
+  switch (argName) {
+    case 'mediajelAppId':
+      mediajelAppId = argValue;
+      break;
+    case 'environment':
+      environment = argValue.toLowerCase();
+      break;
+    case 'test':
+      production = false;
   }
-
-  if (!src) {
-    continue;
-  }
-}
+});
 
 if (mediajelAppId) {
   setTrackerConfig(mediajelAppId, environment, production);
