ee2e0e3bff1c7a7935b84a593921a8e8b6e32ad0
Pacholo Amit <69985528+pacholoamit@users.noreply.github.com>
2021-06-10 17:23:50 +0800

    Implentation of .env  file for values

--- deploy/postdeploy.js
+++ deploy/postdeploy.js
@@ -1,37 +1,37 @@
-var awsCli = require('aws-cli-js');
-var Options = awsCli.Options;
-var Aws = awsCli.Aws;
+require('dotenv').config();
+const awsCli = require('aws-cli-js');
+const Options = awsCli.Options;
+const Aws = awsCli.Aws;
 
-var options = new Options(
-  process.env.AWS_ACCESS_KEY,
-  process.env.AWS_SECRET_ACCESS_KEY
+const options = new Options(
+	process.env.AWS_ACCESS_KEY,
+	process.env.AWS_SECRET_ACCESS_KEY,
 );
-var aws = new Aws(options);
 
-var bucket = 's3://mediajel-tracker-staging';
-var distId = 'E1ZEPT1NM5152K';
-var cdnPath = '*';
-var path = '../dist';
+const aws = new Aws(options);
 
-console.log(bucket);
+const bucket = process.env.AWS_S3_BUCKET;
+const distId = process.env.AWS_CLOUDFRONT_DIST_ID;
+const cdnPath = process.env.AWS_CLOUDFRONT_CDN_PATH;
+const path = process.env.DIST_PATH;
 
 (async function () {
-  //Upload to S3 bucket
-  await aws
-    .command(`s3 cp ${path} ${bucket} --recursive`)
-    .then((data) => {
-      console.log('\x1b[36m', data.raw, '\x1b[0m');
-      console.info('\x1b[32m ', '✨ Upload completed!!!', '\x1b[0m');
-    })
-    .catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
+	//Upload to S3 bucket
+	await aws
+		.command(`s3 cp ${path} ${bucket} --recursive`)
+		.then((data) => {
+			console.log('\x1b[36m', data.raw, '\x1b[0m');
+			console.info('\x1b[32m ', '✨ Upload completed!!!', '\x1b[0m');
+		})
+		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
 
-  //Invalidation of cache on Cloudfront CDN
-  await aws
-    .command(
-      `cloudfront create-invalidation \ --distribution-id ${distId} \ --paths ${cdnPath}`
-    )
-    .then((res) => {
-      console.log('\x1b[36m', res, '\x1b[0m');
-    })
-    .catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
+	// Invalidation of cache on Cloudfront CDN
+	await aws
+		.command(
+			`cloudfront create-invalidation \ --distribution-id ${distId} \ --paths ${cdnPath}`,
+		)
+		.then((res) => {
+			console.log('\x1b[36m', res, '\x1b[0m');
+		})
+		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
 })();
--- deploy/predeploy.js
+++ deploy/predeploy.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
-var path = '../dist';
+require('dotenv').config();
+const path = process.env.DIST_PATH;
 
 function predeploy() {
-  fs.rm(path, { recursive: true }, (err) => {
-    if (err) {
-      //Do nothing, again.
-      console.warn('\x1b[33m', `⚠️ No ${path}`, '\x1b[0m');
-      console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
-    } else {
-      //Deletes files if there are any.
-      console.info('\x1b[36m', `⚠️ ${path} deleted successfully `, '\x1b[0m');
-      console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
-    }
-  });
+	fs.rm(path, { recursive: true }, (err) => {
+		if (err) {
+			//Do nothing, again.
+			console.warn('\x1b[33m', `⚠️ No ${path}`, '\x1b[0m');
+			console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
+		} else {
+			//Deletes files if there are any.
+			console.info('\x1b[36m', `⚠️ ${path} deleted successfully `, '\x1b[0m');
+			console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
+		}
+	});
 }
 
 fs.access(path, predeploy);
