13a3176a7f134f822bcb8983687024d15fb2d084
Pacholo Amit <69985528+pacholoamit@users.noreply.github.com>
2021-06-10 19:21:34 +0800

    Changed "cp" command to "sync" to remove any duplication

--- deploy/postdeploy.js
+++ deploy/postdeploy.js
@@ -9,7 +9,7 @@ const aws = new Aws(options);
 (async function () {
 	//Upload to S3 bucket
 	await aws
-		.command(`s3 cp ${env.dist_path} ${env.s3_bucket} --recursive`)
+		.command(`s3 sync ${env.dist_path} ${env.s3_bucket}`)
 		.then((data) => {
 			console.log('\x1b[36m', data.raw, '\x1b[0m');
 			console.info('\x1b[32m ', '✨ Upload completed!!!', '\x1b[0m');
@@ -23,6 +23,8 @@ const aws = new Aws(options);
 		)
 		.then((res) => {
 			console.log('\x1b[36m', res, '\x1b[0m');
+			console.info('\x1b[36m', 'Invalidation of cache completed!', '\x1b[0m');
 		})
-		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
+		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'))
+		.finally(() => console.log('\x1b[45m', 'Process end!!!', '\x1b[0m'));
 })();
