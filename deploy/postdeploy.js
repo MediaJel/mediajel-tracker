c5ba7fd25b410f55c99e4bc67485e80621403aaa
Pacholo Amit <pacholoamit.tech@gmail.com>
2021-06-15 05:18:29 +0800

    deployment script cleanup

--- deploy/postdeploy.js
+++ deploy/postdeploy.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const awsCli = require('aws-cli-js');
 const env = require('./config');
 
@@ -6,25 +7,25 @@ const { Options, Aws } = awsCli;
 const options = new Options(env.access_key, env.secret_key);
 const aws = new Aws(options);
 
-(async function () {
-	//Upload to S3 bucket
-	await aws
-		.command(`s3 sync ${env.dist_path} ${env.s3_bucket}`)
-		.then((data) => {
-			console.log('\x1b[36m', data.raw, '\x1b[0m');
-			console.info('\x1b[32m ', '✨ Upload completed!!!', '\x1b[0m');
-		})
-		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
+(async function postDeploy() {
+  // Upload to S3 bucket
+  await aws
+    .command(`s3 sync ${env.dist_path} ${env.s3_bucket}`)
+    .then((data) => {
+      console.log('\x1b[36m', data.raw, '\x1b[0m');
+      console.info('\x1b[32m ', '✨ Upload completed!!!', '\x1b[0m');
+    })
+    .catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'));
 
-	// Invalidation of cache on Cloudfront CDN
-	await aws
-		.command(
-			`cloudfront create-invalidation \ --distribution-id ${env.cdn_id} \ --paths ${env.cdn_path}`,
-		)
-		.then((res) => {
-			console.log('\x1b[36m', res, '\x1b[0m');
-			console.info('\x1b[36m', 'Invalidation of cache completed!', '\x1b[0m');
-		})
-		.catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'))
-		.finally(() => console.log('\x1b[45m', 'Process end!!!', '\x1b[0m'));
-})();
+  // Invalidation of cache on Cloudfront CDN
+  await aws
+    .command(
+      `cloudfront create-invalidation  --distribution-id ${env.cdn_id}  --paths ${env.cdn_path}`,
+    )
+    .then((res) => {
+      console.log('\x1b[36m', res, '\x1b[0m');
+      console.info('\x1b[36m', 'Invalidation of cache completed!', '\x1b[0m');
+    })
+    .catch((e) => console.warn('\x1b[41m', e, '\x1b[0m'))
+    .finally(() => console.log('\x1b[45m', 'Process end!!!', '\x1b[0m'));
+}());
--- deploy/predeploy.js
+++ deploy/predeploy.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 const env = require('./config');
 
 function predeploy() {
-	fs.rm(env.dist_path, { recursive: true }, (err) => {
-		if (err) {
-			//Do nothing, again.
-			console.warn('\x1b[33m', `⚠️ No ${env.dist_path}`, '\x1b[0m');
-			console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
-		} else {
-			//Deletes files if there are any.
-			console.info(
-				'\x1b[36m',
-				`⚠️ ${env.dist_path} deleted successfully `,
-				'\x1b[0m',
-			);
-			console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
-		}
-	});
+  fs.rm(env.dist_path, { recursive: true }, (err) => {
+    if (err) {
+      // Do nothing, again.
+      console.warn('\x1b[33m', `⚠️ No ${env.dist_path}`, '\x1b[0m');
+      console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
+    } else {
+      // Deletes files if there are any.
+      console.info(
+        '\x1b[36m',
+        `⚠️ ${env.dist_path} deleted successfully `,
+        '\x1b[0m',
+      );
+      console.log('\x1b[36m', '✨ Proceeding with build', '\x1b[0m');
+    }
+  });
 }
 
 fs.access(env.dist_path, predeploy);
