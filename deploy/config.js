8a363ecdd4097b2117875ceae91e633d75dce9f5
Pacholo Amit <pacholoamit.tech@gmail.com>
2021-06-16 04:00:43 +0800

    Update env files

--- deploy/config.js
+++ deploy/config.js
@@ -2,12 +2,12 @@
 require('dotenv').config();
 
 const config = {
-  access_key: process.env.AWS_ACCESS_KEY,
+  access_key: process.env.AWS_ACCESS_KEY_ID,
   secret_key: process.env.AWS_SECRET_ACCESS_KEY,
-  s3_bucket: process.env.AWS_S3_BUCKET,
-  cdn_id: process.env.AWS_CLOUDFRONT_DIST_ID,
-  cdn_path: process.env.AWS_CLOUDFRONT_CDN_PATH,
-  dist_path: process.env.DIST_PATH,
+  s3_bucket: process.env.MJ_TRACKER_AWS_S3_BUCKET,
+  cdn_id: process.env.MJ_TRACKER_AWS_CLOUDFRONT_DIST_ID,
+  cdn_path: process.env.MJ_TRACKER_AWS_CLOUDFRONT_CDN_PATH,
+  dist_path: process.env.MJ_TRACKER_DIST_PATH,
 };
 
 module.exports = config;
