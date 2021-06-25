9187aa1d1d1d565e31944ad7252ebf4ccd400181
Pacholo Amit <pacholoamit.tech@gmail.com>
2021-06-26 01:15:15 +0800

    Update Shopify.js

--- src/TrackerConfig/Platforms/Shopify.js
+++ src/TrackerConfig/Platforms/Shopify.js
@@ -1,12 +1,12 @@
 export default function Shopify(aid) {
   const mediajelAppId = aid;
   // Note: Tracks only Transactions
-
+  //This does not work
   window.mediajelAppId(
     "addTrans",
-    "{{order.id}}",
+    {{order.id}},
     `${mediajelAppId}`,
-    "{{order.total_price}}",
+    {{order.total_price}},
     "0",
     "0",
     "N/A",
