d71251dad0b421d3ba32b3d0d474d8926c054e0c
vancebaritugo <vancebaritugo@gmail.com>
2021-07-15 06:11:12 +0800

    Update dynamicImport.js

--- src/trackerConfig/utils/dynamicImport.js
+++ src/trackerConfig/utils/dynamicImport.js
@@ -1,16 +1,15 @@
 export default async function dynamicImport(environment) {
-  console.log(`Importing ${environment}...`);
   switch (environment) {
     case "jane": {
-        const { default: func } = await import("../platforms/jane");
-        func();
-        break;
-      }
+      const { default: func } = await import("../platforms/jane");
+      func();
+      break;
+    }
     case "dutchie": {
-        const { default: func } = await import("../platforms/dutchie");
-        func();
-        break;
-      }
+      const { default: func } = await import("../platforms/dutchie");
+      func();
+      break;
+    }
     case "meadow": {
       const { default: func } = await import("../platforms/meadow");
       func();
