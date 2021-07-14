3f2cb80512b5a7f83ba67753ba3a954212b51660
vancebaritugo <vancebaritugo@gmail.com>
2021-07-15 05:01:47 +0800

    Added meadow platform
    - added the meadow platform into the universal tag

--- src/TrackerConfig/Utils/DynamicImport.js
+++ src/TrackerConfig/Utils/DynamicImport.js
@@ -1,15 +1,18 @@
-export default async function DynamicImport(env) {
-  switch (env) {
-    case "jane": {
-      const { default: func } = await import("../Platforms/Jane");
+export default async function dynamicImport(environment) {
+  console.log(`Importing ${environment}...`);
+  switch (environment) {
+    case "jane":
+      const { default: func } = await import("../platforms/jane");
       func();
       break;
-    }
-    case "dutchieiframe": {
-      const { default: func } = await import("../Platforms/DutchieIframe");
+    case "dutchie":
+      const { default: func } = await import("../platforms/DutchieIframe");
       func();
       break;
-    }
+    case "meadow":
+      const { default: func } = await import("../platforms/meadow");
+      func();
+      break;
     default:
       console.error("Undefined environment");
       break;
