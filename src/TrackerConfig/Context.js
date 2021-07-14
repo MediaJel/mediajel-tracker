87ba0f0202409a0069ead97f77aee2760a403e87
vancebaritugo <vancebaritugo@gmail.com>
2021-07-15 05:49:57 +0800

    Revert "Revert "Delete src directory""
    This reverts commit f10c456e25b4aee0c51d95884bb1b4ce0cd8d497.

--- src/TrackerConfig/Context.js
+++
@@ -1,8 +0,0 @@
-export default class Context {
-  // Todo: add default col to env file
-  constructor(appId, environment, collector = "//collector.dmp.cnna.io") {
-    this.appId = appId;
-    this.environment = environment;
-    this.collector = collector;
-  }
-}
