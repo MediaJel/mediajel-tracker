be664aedd32c4dcbc18c58164d7f16d6f02454db
vancebaritugo <81504441+vancebaritugo@users.noreply.github.com>
2021-07-15 05:47:46 +0800

    Delete src directory

--- src/TrackerConfig/handleTag.js
+++
@@ -1,9 +0,0 @@
-import pageview from "./types/pageview";
-import dynamicImport from "./utils/dynamicImport";
-
-export default function handleTag({ appId, environment, collector }) {
-  pageview(appId, collector);
-  if (environment) {
-    dynamicImport(environment);
-  }
-}
