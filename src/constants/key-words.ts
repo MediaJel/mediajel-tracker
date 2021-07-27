1cf272c667303ac86a0f3f91125f62d0e558b3a2
Pacholo Amit <pacholoamit.tech@gmail.com>
2021-07-27 23:10:34 +0800

    modify keywords

--- src/constants/key-words.ts
+++ src/constants/key-words.ts
@@ -1,10 +1,10 @@
 // Keywords as criteria to get URL element
 const keyWords: string[] = [
-  process.env.MJ_PRODUCTION_S3_LINK,
-  process.env.MJ_STAGING_S3_LINK,
-  process.env.MJ_PROUCTION_CLOUDFRONT_LINK,
-  process.env.MJ_STAGING_CLOUDFRONT_LINK,
-  process.env.MJ_LOCAL_DEVELOPMENT_URL,
+  "tags.cnna.io",
+  "tags.mediajel.ninja",
+  "tag.cnna.io",
+  "tag.mediajel.ninja",
+  "universal-tag-localhost",
 ];
 
 export { keyWords };
