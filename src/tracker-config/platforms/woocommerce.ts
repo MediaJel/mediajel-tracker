77776b77605f5124d8d04e0013fcc7c0356d7986
vancebaritugo <vancebaritugo@gmail.com>
2021-09-30 06:10:06 +0800

    Added woocommerce platform for testing

---
+++ src/tracker-config/platforms/woocommerce.ts
@@ -0,0 +1,27 @@
+import { EcommerceContext } from "../../interface";
+
+export default function woocommerceTracker(context: EcommerceContext) {
+  const { appId } = context;
+  const order = '<?php echo wc_get_order( $order_id );?>';
+  const id = '<?php echo $order_id;?>';
+  const total = '<?php echo wc_get_order( $order_id )->get_total();?>';
+  const tax = '<?php echo wc_get_order( $order_id )->get_total_tax();?>';
+  const shipping = '<?php echo wc_get_order( $order_id )->get_total_shipping();?>';
+  const city = '<?php echo wc_get_order( $order_id )->get_billing_city();?>';
+  const state = '<?php echo wc_get_order( $order_id )->get_billing_state();?>';
+  const country = '<?php echo wc_get_order( $order_id )->get_billing_country();?>';
+  const currency = '<?php echo wc_get_order( $order_id )->get_currency();?>';
+  window.tracker(
+    'addTrans',
+    id,
+    appId,
+    total,
+    tax,
+    shipping,
+    city,
+    state,
+    country,
+    currency,
+  );
+  window.tracker('trackTrans');
+}
