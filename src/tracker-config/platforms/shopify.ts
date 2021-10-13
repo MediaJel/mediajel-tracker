b2e2682c8aeeb62a06856cc9492afd8229ed576b
vancebaritugo <vancebaritugo@gmail.com>
2021-10-14 03:42:39 +0800

    Added shopify platform to universal tag

---
+++ src/tracker-config/platforms/shopify.ts
@@ -0,0 +1,33 @@
+import { EcommerceContext } from "../../interface";
+
+export default function shopifyTracker(context: EcommerceContext) {
+  const { appId, retailId } = context;
+
+  function receiveMessage(event) {
+    const { data, name } = event.data;
+
+    window.tracker("addTrans",
+      "{{ order.order_number }}",
+      !retailId ? appId : retailId,
+      parseInt("{{ order.total_price }}"),
+      parseInt("{{ order.tax_price }}" ? "{{ order.tax_price }}" : "0"),
+      parseInt("{{ order.shipping_price }}" ? "{{ order.shipping_price }}" : "0"),
+      "{{ customer_address.city }}" ? "{{ customer_address.city }}" : "N/A",
+      "{{ customer_address.province }}" ? "{{ customer_address.province }}" : "N/A",
+      "{{ customer_address.country }}" ? "{{ customer_address.country}}" : "N/A",
+      "{{ currency.iso_code }}"
+    )
+    
+    window.tracker("addItem",
+      "{{ order.order_number }}",
+      "{{ product.variants }}",
+      "{{ product.title }}",
+      "{{ product.first_available_variant }}",
+      parseInt("{{ product.price_min }}"),
+      parseInt("{{ product.available }}" ? "{{ product.available }}" : "1"),
+      "{{ currency.iso_code }}"
+    );
+
+    window.tracker('trackTrans');
+  }
+}
