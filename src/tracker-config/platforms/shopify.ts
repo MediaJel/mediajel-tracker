71c433baacdd15f8c37e65a17c12e3ba56414026
vancebaritugo <vancebaritugo@gmail.com>
2021-10-21 03:14:15 +0800

    Modify shopify integration in universal tag

--- src/tracker-config/platforms/shopify.ts
+++ src/tracker-config/platforms/shopify.ts
@@ -1,28 +1,33 @@
 import { EcommerceContext } from "../../interface";
 
 export default function shopifyTracker(context: EcommerceContext) {
+  const order = document.currentScript.getAttribute('order');
+  const product = document.currentScript.getAttribute('product');
+  const customer_address = document.currentScript.getAttribute('customer_address');
+  const currency = document.currentScript.getAttribute('currency');
+
   const { appId, retailId } = context;
 
   window.tracker("addTrans",
-    "{{ order.order_number }}",
+    order.order_number.toString(),
     !retailId ? appId : retailId,
-    parseInt("{{ order.total_price }}"),
-    parseInt("{{ order.tax_price }}" ? "{{ order.tax_price }}" : "0"),
-    parseInt("{{ order.shipping_price }}" ? "{{ order.shipping_price }}" : "0"),
-    "{{ customer_address.city }}" ? "{{ customer_address.city }}" : "N/A",
-    "{{ customer_address.province }}" ? "{{ customer_address.province }}" : "N/A",
-    "{{ customer_address.country }}" ? "{{ customer_address.country}}" : "N/A",
-    "{{ currency.iso_code }}"
+    parseInt(order.total_price),
+    parseInt(order.tax_price ? order.tax_price : 0),
+    parseInt(order.shipping_price ? order.shipping_price : 0),
+    (customer_address.city ?  customer_address.city : "N/A").toString(),
+    (customer_address.province ? customer_address.province : "N/A").toString(),
+    (customer_address.country ?  customer_address.country : "N/A").toString(),
+    currency.iso_code.toString()
   )
   
   window.tracker("addItem",
-    "{{ order.order_number }}",
-    "{{ product.variants }}",
-    "{{ product.title }}",
-    "{{ product.first_available_variant }}",
-    parseInt("{{ product.price_min }}"),
-    parseInt("{{ product.available }}" ? "{{ product.available }}" : "1"),
-    "{{ currency.iso_code }}"
+    order.order_number.toString(),
+    product.variants.toString(),
+    product.title.toString(),
+    product.first_available_variant.toString(),
+    parseInt(product.price_min),
+    parseInt(product.available ? product.available : 1),
+    currency.iso_code.toString()
   );
 
   window.tracker('trackTrans');
