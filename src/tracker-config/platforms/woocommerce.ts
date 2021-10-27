c54583d55932871a2779285fb44776008d49495a
vancebaritugo <vancebaritugo@gmail.com>
2021-10-28 07:12:16 +0800

    Added checking shopify and woocommerce, also added woocommerce transaction items

--- src/tracker-config/platforms/woocommerce.ts
+++ src/tracker-config/platforms/woocommerce.ts
@@ -6,19 +6,32 @@ export default function woocommerceTracker(context: EcommerceContext) {
   if(window.location.pathname.includes("/checkout/order-received/") && window.location.search) {
     const scripts = document.getElementById('universal');
     const transaction = JSON.parse(scripts.getAttribute('data-order'));
+    const products = JSON.parse(scripts.getAttribute('data-product'));
     
-    window.tracker(
-      'addTrans',
+    window.tracker("addTrans",
       (transaction.id).toString(),
       !retailId ? appId : retailId,
       parseFloat(transaction.total),
-      parseFloat(transaction.total_tax),
-      parseFloat(transaction.shipping_total),
-      (transaction.billing.city).toString(),
-      (transaction.billing.state).toString(),
-      (transaction.billing.country).toString(),
-      (transaction.currency).toString(),
+      parseFloat(transaction.total_tax ? transaction.total_tax : 0),
+      parseFloat(transaction.shipping_total ? transaction.shipping_total : 0),
+      (transaction.billing.city ? transaction.billing.city : "N/A").toString(),
+      (transaction.billing.state ? transaction.billing.state : "N/A").toString(),
+      (transaction.billing.country ? transaction.billing.country : "N/A").toString(),
+      (transaction.currency ? transaction.currency : "USD").toString(),
     );
+
+    for(let i = 0; i < products.length; ++i) {
+      window.tracker("addItem",
+        transaction.order_id.toString(),
+        products[i].id.toString(),
+        (products[i].name ? products[i].name : "N/A").toString(),
+        (products[i].category ? products[i].category : "N/A").toString(),
+        parseFloat(products[i].total),
+        parseFloat(products[i].quantity ? products[i].quantity : 1),
+        (transaction.currency ? transaction.currency : "USD").toString()
+      );
+    }
+
     window.tracker('trackTrans');
   }
   else {
