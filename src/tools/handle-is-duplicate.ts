beeaaa46287184b3cc86f9b8afced55c27fc67e9
pacholoamit <pacholoamit.tech@gmail.com>
2021-11-17 02:59:21 +0800

    Update handle-is-duplicate.ts

--- src/tools/handle-is-duplicate.ts
+++ src/tools/handle-is-duplicate.ts
@@ -4,6 +4,7 @@ import { throwError } from "./throw-error";
 // Checks if present candidate scripts is more than 1
 export function handleIsDuplicate(context: ContextArg[]): ContextArg[] {
   if (context.length > 1) {
+    console.log(context);
     throwError({ name: "handleIsDuplicate:", cause: "Duplicate string" });
   } else {
     return context;
