404c700cc781cc78efb42d70966d95a7baa0306c
vancebaritugo <vancebaritugo@gmail.com>
2021-11-17 00:23:56 +0800

    first working version of getting transactions for tymber checkpoint 1

--- src/tracker-config/platforms/tymber.ts
+++ src/tracker-config/platforms/tymber.ts
@@ -4,31 +4,48 @@ export default function tymberTracker(context: EcommerceContext) {
   const { appId, retailId } = context;
 
   (function () {
-    var origOpen = XMLHttpRequest.prototype.open;
+    const origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
       this.addEventListener("load", function () {
-        var response = this.responseText;
+        const response = this.responseText;
 
         if (this.responseURL.includes("?delivery_type=")) {
-          if(!response.data) return;
-          var responseData = JSON.parse(response.data);
+          const responseData = JSON.parse(response);
 
-          if (responseData.type !== "orders") return;
-          var transaction = responseData.attributes;
-          window.tracker(
-            "addTrans",
-            transaction.order_number.toString(),
-            !retailId ? appId : retailId,
-            parseFloat(transaction.total.amount) / 100,
-            parseFloat(transaction.tax_total.amount || 0) / 100,
-            parseFloat(transaction.delivery_fee.amount || 0) / 100,
-            (transaction.delivery_address.city || "N/A").toString(),
-            (transaction.delivery_address.state || "N/A").toString(),
-            (transaction.delivery_address.country || "US").toString(),
-            (transaction.total.currency || "USD").toString()
-          );
+          if (!responseData.data) return;
+          if (responseData.data.type === "orders") {
+            const transaction = responseData.data.attributes;
+            const products = responseData.included;
+            const item = products.attributes;
+            window.tracker(
+              "addTrans",
+              transaction.order_number.toString(),
+              !retailId ? appId : retailId,
+              parseFloat(transaction.total.amount) / 100,
+              parseFloat(transaction.tax_total.amount || 0) / 100,
+              parseFloat(transaction.delivery_fee.amount || 0) / 100,
+              (transaction.delivery_address.city || "N/A").toString(),
+              (transaction.delivery_address.state || "N/A").toString(),
+              (transaction.delivery_address.country || "US").toString(),
+              (transaction.total.currency || "USD").toString()
+            );
 
-          window.tracker("trackTrans")
+            for(let i = 0; i < products.length; ++i) {
+              if (products[i].type === "products") {
+                window.tracker("addItem",
+                  transaction.order_number.toString(),
+                  (item[i].sku || item[i].id).toString(),
+                  (item[i].name ? products[i].title : "N/A").toString(),
+                  (item[i].variant_title ? products[i].variant_title : "N/A").toString(),
+                  parseFloat(products[i].price),
+                  parseFloat(products[i].quantity ? products[i].quantity : 1),
+                  (transaction.currency ? transaction.currency : "USD").toString()
+                );
+              }
+            }
+
+            window.tracker("trackTrans")
+          }
         }
       });
       origOpen.apply(this, arguments);
