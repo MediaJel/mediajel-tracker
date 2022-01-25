552de1f19479444298f622b9c8c248fa7108f414
vancebaritugo <vancebaritugo@gmail.com>
2022-01-26 00:05:18 +0800

    Refactored code for jane checkpoint

--- src/apply-tag/helpers/platforms/jane.ts
+++ src/apply-tag/helpers/platforms/jane.ts
@@ -7,54 +7,28 @@ const janeTracker = ({
   function receiveMessage(event) {
     const { payload, messageType } = event.data;
 
-    if (!payload || messageType !== "analyticsEvent") return;
-
-    if (payload.name === "checkout") {
-      const { products, cartId, estimatedTotal } = payload.properties;
-      window.tracker(
-        "addTrans",
-        cartId,
-        !retailId ? appId : retailId,
-        estimatedTotal,
-        "0",
-        "0",
-        "N/A",
-        "California",
-        "USA",
-        "USD"
-      );
-
-      for (let i = 0, l = products.length; i < l; i += 1) {
-        const item = products[i];
-
-        window.tracker(
-          "addItem",
-          cartId,
-          item.product_id,
-          item.name,
-          item.category,
-          item.unit_price,
-          item.count
-        );
-      }
-
-      window.tracker("trackTrans");
+    if (!payload || messageType !== "analyticsEvent") {
+      return;
     }
+
     if (payload.name === "cartItemAdd") {
       const { product, productId } = payload.properties;
+      
       window.tracker(
         "trackAddToCart",
         productId.toString(),
-        product.name,
-        product.category,
-        product.unit_price ?? 0,
-        product.count ?? 1,
+        (product.name || "N/A").toString(),
+        (product.category || "N/A").toString(),
+        parseFloat(product.unit_price || 0),
+        parseInt(product.count || 1),
         "USD"
       );
     }
+
     if (payload.name === "cartItemRemoval") {
       const { productId } = payload.properties;
 
+      // Hardcoded because most fields are empty
       window.tracker(
         "trackRemoveFromCart",
         productId.toString(),
@@ -65,7 +39,40 @@ const janeTracker = ({
         "USD"
       );
     }
+
+    if (payload.name === "checkout") {
+      const { products, cartId, estimatedTotal, deliveryFee, deliveryAddress, salesTax, storeTax } = payload.properties;
+      
+      window.tracker(
+        "addTrans",
+        cartId.toString(),
+        retailId || appId,
+        parseFloat(estimatedTotal),
+        parseFloat(salesTax + storeTax || 0),
+        parseFloat(deliveryFee || 0),
+        (deliveryAddress.city || "N/A").toString(),
+        (deliveryAddress.state_code || "N/A").toString(),
+        (deliveryAddress.country_code || "N/A").toString(),
+        "USD"
+      );
+
+      for (let i = 0; i < products.length; ++i) {
+        window.tracker(
+          "addItem",
+          cartId.toString(),
+          (products[i].product_id).toString(),
+          (products[i].name || "N/A").toString(),
+          (products[i].category || "N/A").toString(),
+          parseFloat(products[i].unit_price || 0),
+          parseInt(products[i].count || 1),
+          "USD"
+        );
+      }
+
+      window.tracker("trackTrans");
+    }
   }
+  
   window.addEventListener("message", receiveMessage, false);
 };
 
