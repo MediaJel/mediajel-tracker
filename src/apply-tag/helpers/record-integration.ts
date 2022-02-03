aef3c73983ab2494206a18a797eec08148f89d31
pacholoamit <pacholoamit.tech@gmail.com>
2022-02-03 23:57:07 +0800

    minor refactor

--- src/apply-tag/helpers/record-integration.ts
+++ src/apply-tag/helpers/record-integration.ts
@@ -5,7 +5,7 @@ import {TagContext} from '../../shared/types'
 export default recordIntegration = ({appId, environment}): Pick<TagContext, 'appId' | 'environment'> => {
 
     const recordSchema = {
-        schema: "iglu:com.mediajel.events/record/jsonschema/1-0-0"
+        schema: "iglu:com.mediajel.events/record/jsonschema/1-0-0",
         data: {
             appId: appId,
             cart: environment
