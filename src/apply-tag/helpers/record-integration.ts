ab4f475cdecf76653b344e2d5f6b3d8c34299868
pacholoamit <pacholoamit.tech@gmail.com>
2022-02-04 01:13:09 +0800

    increment record

--- src/apply-tag/helpers/record-integration.ts
+++ src/apply-tag/helpers/record-integration.ts
@@ -5,7 +5,7 @@ import { TagContext } from '../../shared/types'
 const recordIntegration = ({ appId, environment }: Pick<TagContext, 'appId' | 'environment'>) => {
 
     const recordSchema = {
-        schema: "iglu:com.mediajel.events/record/jsonschema/1-0-0",
+        schema: "iglu:com.mediajel.events/record/jsonschema/1-0-1",
         data: {
             appId: appId,
             cart: environment
