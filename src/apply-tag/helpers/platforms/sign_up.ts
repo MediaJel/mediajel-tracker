fbc8f0b4efa7576c9ba43df5c4282624ae9d8202
vancebaritugo <vancebaritugo@gmail.com>
2022-02-23 04:39:46 +0800

    Remove test for comparing data of javascript tracker vs image pixel for signups

--- src/apply-tag/helpers/platforms/sign_up.ts
+++ src/apply-tag/helpers/platforms/sign_up.ts
@@ -14,6 +14,4 @@ export default function liquidm() {
       advertiser: "advertiser"
     }
   });
-
-  fetch("https://collector.dmp.cnna.io/com.snowplowanalytics.iglu/v1?schema=iglu:com.mediajel.events/sign_up/jsonschema/1-0-1&aid=Testing&uuid=uuid&firstName=firstName&lastName=lastName&gender=gender&emailAddress=emailAddress&address=address&city=city&state=state&phoneNumber=phoneNumber&advertiser=advertiser");
 }
