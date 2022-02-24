7532ce1cfaa4323d3442db3c3a80cf3fc4918336
vancebaritugo <vancebaritugo@gmail.com>
2022-02-25 03:44:10 +0800

    Redo testing for javascript tracker vs image pixel tracker

--- src/apply-tag/helpers/platforms/sign_up.ts
+++ src/apply-tag/helpers/platforms/sign_up.ts
@@ -1,3 +1,5 @@
+import { createHash } from 'crypto';
+
 export default function liquidm() {
   window.tracker("trackSelfDescribingEvent", {
     schema: "iglu:com.mediajel.events/sign_up/jsonschema/1-0-1",
@@ -14,4 +16,6 @@ export default function liquidm() {
       advertiser: "advertiser"
     }
   });
+
+  fetch("https://collector.dmp.mediajel.ninja/com.snowplowanalytics.iglu/v1?schema=iglu:com.mediajel.events/sign_up/jsonschema/1-0-1&aid=TestingSignUps&firstName={{FirstName}}&lastName={{lastName}}&gender={{gender}}&emailAddress={{emailAddress}}&address={{address}}&city={{city}}&state={{state}}&phoneNumber={{phoneNumber}}");
 }
