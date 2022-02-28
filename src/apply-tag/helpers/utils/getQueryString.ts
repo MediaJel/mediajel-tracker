02964fcedfe5b2897a638279c25e814bf2778256
vancebaritugo <vancebaritugo@gmail.com>
2022-03-01 02:52:44 +0800

    Use old method of getQueryString

--- src/apply-tag/helpers/utils/getQueryString.ts
+++ src/apply-tag/helpers/utils/getQueryString.ts
@@ -3,11 +3,13 @@ const getQueryString = () => {
   const index = scripts.length - 1;
   const myScript = scripts[index];
   const querystring: string = myScript.src.substring(myScript.src.indexOf("?"));
+  const params = new URLSearchParams( querystring );
+  const queryStringResult: any = Object.fromEntries((params as any).entries());
 
   // Gets the query string from the script tag
-  const queryStringResult = new Proxy (new URLSearchParams(querystring), {
-    get: (searchParams, prop) => searchParams.get(prop as string),
-  })
+  // const queryStringResult = new Proxy (new URLSearchParams(querystring), {
+  //   get: (searchParams, prop) => searchParams.get(prop as string),
+  // })
 
   return queryStringResult;
 }
