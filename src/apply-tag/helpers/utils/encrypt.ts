68b883160bfad1022f3f6c0b35d8f24faa82a17b
vancebaritugo <vancebaritugo@gmail.com>
2022-03-03 02:23:38 +0800

    Fix import issue with the built-in NodeJs crypto package

--- src/apply-tag/helpers/utils/encrypt.ts
+++ src/apply-tag/helpers/utils/encrypt.ts
@@ -1,7 +1,7 @@
-import { createHmac } from "crypto";
+import * as crypto from "crypto";
 
 const encrypt = (salt: string, data: string, secret = "cnna") => {
-  return createHmac('sha256', secret).update(salt + data).digest("hex");
+  return crypto.createHmac('sha256', secret).update(salt + data).digest("hex");
 }
 
 export default encrypt;
