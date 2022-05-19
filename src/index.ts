f6ab4b023b5f25d801674fdd5f7515256de88e1c
pacholoamit <pacholoamit.tech@gmail.com>
2022-05-20 06:28:02 +0800

    add dynamic import for corresponding version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import applyV1 from "./v1/";
+
 import getContext from "./shared/utils/get-context"
 import { QueryStringContext } from "./shared/types";
 
 (async (): Promise<void> => {
   try {
     const context: QueryStringContext = getContext();
-    if (context.version === "v1") applyV1(context);
+
+    switch (context.version) {
+      case "v1": {
+        const { default: call } = await import("./v1");
+        call(context);
+      }
+    }
 
   } catch (err) {
     const clientError = `An error has occured, please contact your pixel provider: `;
