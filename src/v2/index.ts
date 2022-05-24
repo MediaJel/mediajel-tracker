ec75e786ad20f6064ff7e7febf9117eeaa22ae1b
pacholoamit <pacholoamit.tech@gmail.com>
2022-05-25 00:00:37 +0800

    separate partner pixels

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,10 +1,16 @@
 import createTracker from "./snowplow/create-tracker";
 import { QueryStringContext } from "../shared/types";
 import { debuggerPlugin } from "./snowplow/plugins";
+import { liquidMRetargetingPixel } from "./partners/liquidm/retargeting-pixel";
+import { tapadCookieSyncPixel } from "./partners/tapad/cookie-sync-pixel";
+import { tapadHashSyncPixel } from "./partners/tapad/hash-sync-pixel";
 
 
 const applyV2 = (context: QueryStringContext): void => {
     createTracker(context);
+    liquidMRetargetingPixel()
+    tapadCookieSyncPixel()
+    tapadHashSyncPixel()
 
     // For debugging
     if (context.test === "true") {
