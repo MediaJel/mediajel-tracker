763a390c0d67331581d89b3a8f6919c132308bb6
pacholoamit <pacholoamit.tech@gmail.com>
2022-05-27 05:15:13 +0800

    enable debugger plugin to always run regardless if test is true

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -6,16 +6,13 @@ import { tapadCookieSyncPixel } from "./partners/tapad/cookie-sync-pixel";
 import { tapadHashSyncPixel } from "./partners/tapad/hash-sync-pixel";
 
 const applyV2 = (context: QueryStringContext): void => {
-  createTracker(context); // Creates the tracker with the configuration
-  liquidMRetargetingPixel(); // Calls the LiquidM retargeting pixel
-  tapadCookieSyncPixel(); // Calls the Tapad cookie sync pixel
-  tapadHashSyncPixel(); // Calls the Tapad hash sync pixel
+  createTracker(context);
+  liquidMRetargetingPixel();
+  tapadCookieSyncPixel();
+  tapadHashSyncPixel();
 
-  console.log({ context });
-  // For debugging
-  if (context.test === "true") {
-    debuggerPlugin();
-  }
+  // For debugging in the console
+  debuggerPlugin();
 
   /**
    * If not event is provided, By default use environment.
