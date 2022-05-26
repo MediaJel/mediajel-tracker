006c43945ced121ad6e31692352fcf7335aa3412
pacholoamit <pacholoamit.tech@gmail.com>
2022-05-27 05:25:40 +0800

    add recordIntegration to pageview

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,12 +1,14 @@
-import createTracker from "./snowplow/create-tracker";
+import createTracker from "./snowplow/events/create-tracker";
 import { QueryStringContext } from "../shared/types";
 import { debuggerPlugin } from "./snowplow/plugins";
 import { liquidMRetargetingPixel } from "./partners/liquidm/retargeting-pixel";
 import { tapadCookieSyncPixel } from "./partners/tapad/cookie-sync-pixel";
 import { tapadHashSyncPixel } from "./partners/tapad/hash-sync-pixel";
+import recordIntegration from "./snowplow/events/record";
 
 const applyV2 = (context: QueryStringContext): void => {
   createTracker(context);
+  recordIntegration(context);
   liquidMRetargetingPixel();
   tapadCookieSyncPixel();
   tapadHashSyncPixel();
