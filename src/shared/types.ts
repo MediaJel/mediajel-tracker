be97d696b8585c9bacc87ab1de9a74a14aed642d
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-01 19:20:30 +0800

    Added debugger query param to enable debugger for version 2 of universal tag

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,41 +1,40 @@
-
 // Data structure of the tag after parsing
 export type TransactionParams = {
   environment: string;
   retailId: string;
 };
 
-
 export type SignupParams = {
-  uuid: string,
-  firstName: string,
-  lastName: string,
-  gender: string,
-  emailAddress: string,
-  hashedEmailAddress: string,
-  address: string,
-  city: string,
-  state: string,
-  phoneNumber: string,
-  advertiser: string
-}
+  uuid: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  emailAddress: string;
+  hashedEmailAddress: string;
+  address: string;
+  city: string;
+  state: string;
+  phoneNumber: string;
+  advertiser: string;
+};
 
 export type SnowplowParams = {
-  appId?: string
-  mediajelAppId?: string
-  environment: string
-  event: string
-  test: string
-  version: string
-}
+  appId?: string;
+  mediajelAppId?: string;
+  environment: string;
+  event: string;
+  test: string;
+  version: string;
+};
 
-export type QueryStringParams = Partial<TransactionParams> & Partial<SignupParams> & SnowplowParams
+export type SnowplowPluginParams = {
+  debugger: string;
+};
+
+export type QueryStringParams = Partial<TransactionParams> &
+  Partial<SignupParams> &
+  Partial<SnowplowPluginParams> &
+  SnowplowParams;
 
 // Params available to the tag's query string
-export type QueryStringContext = QueryStringParams & { collector: string; };
-
-
-
-
-
-
+export type QueryStringContext = QueryStringParams & { collector: string };
