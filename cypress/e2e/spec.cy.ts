cf64a9cc16ff4ffb31d29dc3b293cb594c7d2d34
pacholoamit <pacholoamit.tech@gmail.com>
2022-09-30 17:28:19 +0800

    Add more e2e test cases for testing staging & production tag

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,4 @@
-describe("Ensures universal tag does not error", () => {
+describe("Ensure locally compiled universal tag is loaded", () => {
   it("Navigates to test-dev server", () => {
     cy.visit("http://localhost:1234");
   });
@@ -7,13 +7,25 @@ describe("Ensures universal tag does not error", () => {
     cy.get("script[src*='localhost:3000/index.js?']").should("exist");
   });
 
+  it("Checks for Snowplow XHR POST requests on production", () => {
+    cy.intercept("POST", "http://collector.dmp.cnna.io/com.snowplowanalytics.snowplow/tp2").as("production");
+    cy.wait("@production").then((intercept) => {
+      console.log(intercept.response.statusCode);
+      expect(intercept.response.statusCode).to.equal(200);
+    });
+  });
+
   it("Loads snowplow sp.js", () => {
     cy.get("script").should("have.attr", "src", "//dm2q9qfzyjfox.cloudfront.net/sp.js");
   });
-
-  it("Checks for Snowplow XHR POST requests on production", () => {
-    cy.intercept("POST", "http://collector.dmp.cnna.io/com.snowplowanalytics.snowplow/tp2", (req) => {
-      req.reply({ statusCode: 200 });
-    });
-  });
 });
+
+// describe("Ensure locally compiled universal tag is sending events to Snowplow collectors", () => {
+// it("Checks for Snowplow XHR POST requests on staging (&test=true)", () => {
+//   cy.intercept("POST", "http://collector.dmp.mediajel.ninja/com.snowplowanalytics.snowplow/tp2").as("staging");
+//   cy.wait("@staging").then((intercept) => {
+//     console.log(intercept.response.statusCode);
+//     expect(intercept.response.statusCode).to.equal(200);
+//   });
+// });
+// });
