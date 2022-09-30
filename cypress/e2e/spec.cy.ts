da476eb70a21ffacbdf8b47da55fc72b54c5c8ff
pacholoamit <pacholoamit.tech@gmail.com>
2022-09-30 17:51:19 +0800

    Add additional test to check for data sent to collectors

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,15 +7,20 @@ describe("Ensure locally compiled universal tag is loaded", () => {
     cy.get("script[src*='localhost:3000/index.js?']").should("exist");
   });
 
-  it("Ensure locally compiled universal tag is sending events to Production snowplow collector", () => {
-    cy.intercept("POST", "http://collector.dmp.cnna.io/com.snowplowanalytics.snowplow/tp2").as("production");
-    cy.wait("@production").then((intercept) => {
-      console.log(intercept.response.statusCode);
-      expect(intercept.response.statusCode).to.equal(200);
-    });
-  });
-
   it("Loads snowplow sp.js", () => {
     cy.get("script").should("have.attr", "src", "//dm2q9qfzyjfox.cloudfront.net/sp.js");
+  });
+});
+
+describe("Ensure locally compiled universal tag is sending events to Snowplow collectors", () => {
+  it("Checks for Snowplow XHR POST requests on production", () => {
+    cy.intercept("POST", "http://collector.dmp.cnna.io/com.snowplowanalytics.snowplow/tp2").as("production");
+    cy.visit("http://localhost:1234");
+
+    cy.wait("@production").then((intercept) => {
+      console.log(intercept.request.body);
+      expect(intercept.response.statusCode).to.equal(200);
+      expect(intercept.request.body.data[0].aid).to.equal("universal-tag-staging-test");
+    });
   });
 });
