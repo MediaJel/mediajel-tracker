eb860127d12869f7689fa62c699a356e9e1da07e
pacholoamit <pacholoamit.tech@gmail.com>
2022-09-30 17:31:04 +0800

    modify production snowplow collector test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,7 +7,7 @@ describe("Ensure locally compiled universal tag is loaded", () => {
     cy.get("script[src*='localhost:3000/index.js?']").should("exist");
   });
 
-  it("Checks for Snowplow XHR POST requests on production", () => {
+  it("Ensure locally compiled universal tag is sending events to Production snowplow collector", () => {
     cy.intercept("POST", "http://collector.dmp.cnna.io/com.snowplowanalytics.snowplow/tp2").as("production");
     cy.wait("@production").then((intercept) => {
       console.log(intercept.response.statusCode);
@@ -19,13 +19,3 @@ describe("Ensure locally compiled universal tag is loaded", () => {
     cy.get("script").should("have.attr", "src", "//dm2q9qfzyjfox.cloudfront.net/sp.js");
   });
 });
-
-// describe("Ensure locally compiled universal tag is sending events to Snowplow collectors", () => {
-// it("Checks for Snowplow XHR POST requests on staging (&test=true)", () => {
-//   cy.intercept("POST", "http://collector.dmp.mediajel.ninja/com.snowplowanalytics.snowplow/tp2").as("staging");
-//   cy.wait("@staging").then((intercept) => {
-//     console.log(intercept.response.statusCode);
-//     expect(intercept.response.statusCode).to.equal(200);
-//   });
-// });
-// });
