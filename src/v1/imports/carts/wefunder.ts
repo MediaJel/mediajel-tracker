47579dd2f3255c7c61f7f855b81fb85f263116ed
pacholoamit <pacholoamit.tech@gmail.com>
2022-10-26 03:10:53 +0800

    Add XHR

--- src/v1/imports/carts/wefunder.ts
+++ src/v1/imports/carts/wefunder.ts
@@ -1,18 +1,22 @@
 import { datalayerSource } from "../../../shared/sources/google-datalayer-source";
+import { xhrSource } from "../../../shared/sources/xhr-source";
 import { QueryStringContext } from "../../../shared/types";
 
 const wefunderTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
-  datalayerSource((data) => {
-    console.log(data.event);
-    if (data?.event === "conversion") {
+  xhrSource((xhr) => {
+    console.log("XHR" + xhr.responseURL);
+    if (xhr.responseURL.includes("confirmation")) {
+      const data = JSON.parse(xhr?.responseText);
+
+      window.tracker("setUserId", data?.current_user?.email || data?.investment?.investor?.name);
+
       window.tracker(
         "addTrans",
-        data?.eventModel?.transaction_id,
+        data?.investment?.id.toString(),
         retailId ?? appId,
-        parseFloat(data?.eventModel?.value),
+        parseFloat(data?.investment?.amount),
         0,
-        0,
-        "USD"
+        0
       );
 
       window.tracker("trackTrans");
