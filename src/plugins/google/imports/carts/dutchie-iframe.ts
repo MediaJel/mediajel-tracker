f50df2bfb6769592160afb89b0e0b940642a7a6e
pacholoamit <pacholoamit.tech@gmail.com>
2022-11-02 03:50:03 +0800

    remove unrequiredEvents

--- src/plugins/google/imports/carts/dutchie-iframe.ts
+++ src/plugins/google/imports/carts/dutchie-iframe.ts
@@ -5,36 +5,7 @@ interface Context extends GoogleAdsPluginParams, Pick<SnowplowParams, "environme
 
 const dutchieIframeGoogleAds = (context: Context) => {
   dutchieIframeEvents({
-    addToCartEvent(cartEvent) {
-      console.log("Added to cart");
-      window.gtag("event", "add_to_cart", {
-        items: [
-          {
-            id: cartEvent.sku,
-            name: cartEvent.name,
-            category: cartEvent.category,
-            price: cartEvent.unitPrice,
-            quantity: cartEvent.quantity,
-          },
-        ],
-      });
-    },
-    removeFromCartEvent(cartEvent) {
-      console.log("Removed from cart");
-      window.gtag("event", "remove_from_cart", {
-        items: [
-          {
-            id: cartEvent.sku,
-            name: cartEvent.name,
-            category: cartEvent.category,
-            price: cartEvent.unitPrice,
-            quantity: cartEvent.quantity,
-          },
-        ],
-      });
-    },
     transactionEvent(transactionEvent) {
-      console.log("Transaction event");
       window.gtag("event", "conversion", {
         send_to: `${context.conversionId}/${context.conversionLabel}`,
         value: transactionEvent.total,
