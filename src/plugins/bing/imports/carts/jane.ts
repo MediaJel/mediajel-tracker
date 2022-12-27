13644f36c758e419f89e551f4acf760d10bf5001
pacholoamit <pacholoamit.tech@gmail.com>
2022-12-27 22:50:15 +0800

    Add additional parameters for Bing

--- src/plugins/bing/imports/carts/jane.ts
+++ src/plugins/bing/imports/carts/jane.ts
@@ -8,11 +8,18 @@ const janeBingAds = (context: Context) => {
     transactionEvent(transactionData) {
       console.log("🚀🚀🚀 Jane Transaction Event ", { transactionData });
 
-      window.uetq.push("event", "PRODUCT_PURCHASE", {
-        ecomm_prodid: transactionData.id,
-        ecomm_pagetype: "PURCHASE",
+      window.uetq.push("event", "purchase", {
+        transaction_id: transactionData.id,
+        ecomm_prodid: transactionData.items.map((item) => item.sku),
+        ecomm_pagetype: "purchase",
+        ecomm_totalvalue: transactionData.total,
         revenue_value: transactionData.total,
         currency: "USD",
+        items: transactionData.items.map((item) => ({
+          id: item.sku,
+          quantity: item.quantity,
+          price: item.unitPrice,
+        })),
       });
     },
   });
