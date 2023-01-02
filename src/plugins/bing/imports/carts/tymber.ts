44a9147b5e924d863b233cc4196db5d86e371bd5
pacholoamit <pacholoamit.tech@gmail.com>
2023-01-03 03:18:31 +0800

    Add tymber data sources for Bing and Google

---
+++ src/plugins/bing/imports/carts/tymber.ts
@@ -0,0 +1,28 @@
+import tymberDataSource from "../../../../shared/environment-data-sources/tymber";
+import { BingAdsPluginParams, SnowplowParams } from "../../../../shared/types";
+
+interface Context extends BingAdsPluginParams, Pick<SnowplowParams, "environment"> {}
+
+const tymberBingAds = (context: Context) => {
+  tymberDataSource({
+    transactionEvent(transactionData) {
+      console.log("🚀🚀🚀 tymber Transaction Event ", { transactionData });
+
+      window.uetq.push("event", "purchase", {
+        transaction_id: transactionData.id,
+        ecomm_prodid: transactionData.items.map((item) => item.sku),
+        ecomm_pagetype: "purchase",
+        ecomm_totalvalue: transactionData.total,
+        revenue_value: transactionData.total,
+        currency: "USD",
+        items: transactionData.items.map((item) => ({
+          id: item.sku,
+          quantity: item.quantity,
+          price: item.unitPrice,
+        })),
+      });
+    },
+  });
+};
+
+export default tymberBingAds;
