155b3d9f80a15b9bac12f07e66961c39243e1da7
pacholoamit <pacholoamit.tech@gmail.com>
2023-01-25 00:47:34 +0800

    Add logging

--- src/v1/imports/carts/square.ts
+++ src/v1/imports/carts/square.ts
@@ -2,7 +2,9 @@ import { datalayerSource } from "../../../shared/sources/google-datalayer-source
 import { QueryStringContext } from "../../../shared/types";
 
 const squareTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
-  datalayerSource((data) => {});
+  datalayerSource((data) => {
+    console.log(data);
+  });
 };
 
 export default squareTracker;
