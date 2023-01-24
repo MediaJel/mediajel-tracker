59ee08f62dc7e730845a6777952973d0f6193b06
pacholoamit <pacholoamit.tech@gmail.com>
2023-01-25 01:21:38 +0800

    Add square data source

--- src/v1/imports/carts/square.ts
+++ src/v1/imports/carts/square.ts
@@ -1,6 +1,11 @@
 import { datalayerSource } from "../../../shared/sources/google-datalayer-source";
 import { QueryStringContext } from "../../../shared/types";
 
+/**
+ * The Square Tracker relies on the Google Datalayer Source to track events.
+ * There is documentation to improve this more since Square exports variables
+ * to the window object.
+ */
 const squareTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
   datalayerSource((data) => {
     console.log(data);
