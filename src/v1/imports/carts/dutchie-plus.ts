2eba81f5627e69c2495fb87d0c1adf21f87a3fb4
kimoy catayas <elishakimcatayas@gmail.com>
2023-02-03 03:46:13 +0800

    add dutchie plus cart

---
+++ src/v1/imports/carts/dutchie-plus.ts
@@ -0,0 +1,55 @@
+import { datalayerSource } from "../../../shared/sources/google-datalayer-source";
+import { QueryStringContext } from "../../../shared/types";
+
+const dutchiePlusTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">): void => {
+  datalayerSource((data: any): void => {
+    if (data.event === "purchase") {
+      const { transaction_id, affiliation, value, items } = data.ecommerce;
+
+      window.tracker(
+        "addTrans",
+        transaction_id.toString(),
+        affiliation,
+        parseFloat(value),
+        0,
+        0,
+        "N/A",
+        "N/A",
+        "USA",
+        "USD"
+      );
+
+      for (let i = 0; i < items.length; ++i) {
+        window.tracker(
+          "addItem",
+          items[i].item_id,
+          items[i].item_id,
+          items[i].item_brand,
+          items[i].item_category,
+          items[i].price,
+          items[i].quantity,
+          "USD"
+        );
+      }
+
+      items.forEach((item) => {
+        const { item_id, item_brand, item_category, price, quantity } = item;
+
+        window.tracker(
+          "addItem",
+          item_id.toString(),
+          item_id.toString(),
+          item_brand,
+          item_category,
+          parseFloat(price || 0),
+          parseInt(quantity || 1),
+          "USD"
+        );
+      });
+
+      window.tracker("trackTrans");
+    }
+  });
+};
+
+export default dutchiePlusTracker;
