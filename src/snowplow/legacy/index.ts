9ddc345fc3739b79af6dcc3540102e33faccb190
pacholoamit <pacholoamit.tech@gmail.com>
2023-03-09 20:15:01 +0000

    Add trackAddToCart

--- src/snowplow/legacy/index.ts
+++ src/snowplow/legacy/index.ts
@@ -1,21 +1,51 @@
-import { QueryStringContext, SnowplowBrowserTracker, SnowplowTracker, TransactionEvent } from "/src/shared/types"
-import init from "/src/snowplow/legacy/init"
+import {
+  CartEvent,
+  QueryStringContext,
+  SnowplowBrowserTracker,
+  SnowplowTracker,
+  TransactionEvent,
+} from "/src/shared/types";
+import init from "/src/snowplow/legacy/init";
+
+const trackAddToCart = (tracker: SnowplowBrowserTracker) => {
+  return {
+    trackAddToCart(item: CartEvent) {
+      tracker("trackAddToCart", item.sku, item.name); // Todo: Finish this
+    },
+  };
+};
 
 const trackTransaction = (tracker: SnowplowBrowserTracker) => {
-    return {
-        trackTransaction(transaction: TransactionEvent) {
+  return {
+    trackTransaction(transaction: TransactionEvent) {
+      tracker(
+        "addTrans",
+        transaction.id,
+        "N/A",
+        transaction.total,
+        transaction.tax,
+        transaction.shipping,
+        transaction.city,
+        transaction.state,
+        transaction.country,
+        transaction.currency
+      );
+      transaction.items.forEach((item) => {
+        const { name, category, currency, orderId, quantity, sku, unitPrice } = item;
+        tracker("addItem", orderId, sku, name, category, unitPrice, quantity, currency);
+      });
 
-            tracker("addTrans", transaction.id, "AFFILIATION") // TO BE CONTINUED
-        }
-    }
-}
+      tracker("trackTrans");
+    },
+  };
+};
 
 const createSnowplowLegacyTracker = (context: QueryStringContext): SnowplowTracker => {
-    const tracker = init(context)
+  const tracker = init(context);
 
-    return {
-        ...trackTransaction(tracker),
-    }
-}
+  return {
+    ...trackTransaction(tracker),
+  };
+};
 
-export default createSnowplowLegacyTracker
+export default createSnowplowLegacyTracker;
