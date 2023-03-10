6de987f61c66f891b5a5e76fe1ebd7db8c004329
pacholoamit <pacholoamit.tech@gmail.com>
2023-03-10 22:59:12 +0800

    add docs

--- src/snowplow/index.ts
+++ src/snowplow/index.ts
@@ -1,29 +1,31 @@
 import { SnowplowTracker, SnowplowTrackerInput } from "/src/shared/types";
+import createSnowplowLegacySnowplowTracker from "/src/snowplow/legacy";
 
 /**
- * @description  Creates a Snowplow tracker instance using the legacy
+ * @description  Imports the Snowplow tracker instance using the legacy
  * javascript tracker. This is dynamically imported to reduce the bundle size of the tag.
  *
- * @see {@link createLegacySnowplowTracker}
+ * @see {@link createSnowplowLegacySnowplowTracker}
  *
  * @param {SnowplowTrackerInput} input Input object for the Snowplow tracker
  * @returns {Promise<SnowplowTracker>} Snowplow tracker
  *
  */
-const createLegacySnowplowTracker = async (input: SnowplowTrackerInput): Promise<SnowplowTracker> => {
+const importLegacySnowplowTracker = async (input: SnowplowTrackerInput): Promise<SnowplowTracker> => {
   const { default: legacy } = await import("/src/snowplow/legacy");
   return legacy(input);
 };
 
 /**
- * @description  Creates a Snowplow tracker instance using the standard and supported
+ * @description  Imports the Snowplow tracker instance using the standard and supported
  * javascript tracker. This is dynamically imported to reduce the bundle size of the tag.
  *
+ * @see {@link createStandardSnowplowTracker}
  * @param {SnowplowTrackerInput} input Input object for the Snowplow tracker
  * @returns {Promise<SnowplowTracker>} Snowplow tracker
  *
  */
-const createStandardSnowplowTracker = async (input: SnowplowTrackerInput): Promise<SnowplowTracker> => {
+const importStandardSnowplowTracker = async (input: SnowplowTrackerInput): Promise<SnowplowTracker> => {
   const { default: standard } = await import("/src/snowplow/standard");
   return standard(input);
 };
@@ -40,12 +42,12 @@ const createSnowplowTracker = async (input: SnowplowTrackerInput): Promise<Snowp
   switch (input.version) {
     case "1":
     case "legacy":
-      return await createLegacySnowplowTracker(input);
+      return await importLegacySnowplowTracker(input);
     case "2":
     case "standard":
-      return await createStandardSnowplowTracker(input);
+      return await importStandardSnowplowTracker(input);
     default:
-      return await createLegacySnowplowTracker(input);
+      return await importLegacySnowplowTracker(input);
   }
 };
 
