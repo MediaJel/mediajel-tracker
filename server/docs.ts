10feb5076859eb34a4ae3343ce778979e97fe002
pacholoamit <pacholoamit.tech@gmail.com>
2023-03-11 23:02:00 +0000

    change dir of docs

--- server/docs.ts
+++ server/docs.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 const app = express();
 const port = 3000;
 
-app.use(express.static("docs"));
+app.use(express.static("docs/mediajel-tracker/1.0.0"));
 
 app.listen(port, () => {
   console.log(`Serving docs on ${port}`);
