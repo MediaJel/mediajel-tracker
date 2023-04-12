52caf8d40e65bb043d63bd26ca183a9cccf91efa
kimoy catayas <elishakimcatayas@gmail.com>
2023-04-13 04:11:32 +0800

    use dispense data source

--- src/v1/imports/carts/dispense.ts
+++ src/v1/imports/carts/dispense.ts
@@ -1,42 +1,36 @@
 import { QueryStringContext } from "@/shared/types";
-import { datalayerSource } from "../../../shared/sources/google-datalayer-source";
+import dispenseDataSource from "@/shared/environment-data-sources/dispense";
 
 const dispenseTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">): void => {
-  datalayerSource((data: any): void => {
-    if (data.event === "purchase") {
-      const { transaction_id, affiliation, value, items } = data;
-
+  dispenseDataSource({
+    transactionEvent(transactionData) {
       window.tracker(
         "addTrans",
-        transaction_id.toString(),
-        affiliation,
-        parseFloat(value),
-        0,
-        0,
-        "N/A",
-        "N/A",
-        "USA",
-        "USD"
+        transactionData.id,
+        retailId ?? appId,
+        transactionData.total,
+        transactionData.tax,
+        transactionData.shipping,
+        transactionData.city,
+        transactionData.state,
+        transactionData.country
       );
 
-      items.forEach((item) => {
-        const { item_id, item_name, item_category, price, quantity } = item;
-
+      transactionData.items.forEach((item) => {
         window.tracker(
           "addItem",
-          transaction_id.toString(),
-          item_id.toString(),
-          item_name,
-          item_category,
-          parseFloat(price || 0),
-          parseInt(quantity || 1),
-          "USD"
+          transactionData.id,
+          item.sku,
+          item.name,
+          item.category,
+          item.unitPrice,
+          item.quantity,
+          transactionData.currency
         );
       });
-
       window.tracker("trackTrans");
-    }
-  }, window.gtmDataLayer); // special case for dispense; window.dataLayer is renamed to window.gtmDataLayer
+    },
+  });
 };
 
 export default dispenseTracker;
