5ec80c9182a781ee93baa285951e666288443e34
Kimoy Catayas <elishakimcatayas@gmail.com>
2023-05-27 04:39:09 +0800

    add to imports

--- src/v1/imports/carts/index.ts
+++ src/v1/imports/carts/index.ts
@@ -67,6 +67,10 @@ export default async (context: QueryStringContext): Promise<void> => {
     case "dutchie-plus":
       import("./dutchie-plus").then(({ default: load }): void => load({ appId, retailId }));
       break;
+    //* UNSTABLE
+    case "webjoint":
+      import("./webjoint").then(({ default: load }): void => load({ appId, retailId }));
+      break;
     default:
       throw new Error("Undefined environment");
   }
