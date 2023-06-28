39368ccd621c0d1fbc1ee942eaf15c1a2b1640e4
jonmediajel <jon@mediajel.com>
2023-06-29 05:42:49 +0800

    improvements bigcommerce

--- src/v1/imports/carts/bigcommerce.ts
+++ src/v1/imports/carts/bigcommerce.ts
@@ -1,41 +1,36 @@
 import { xhrResponseSource } from "../../../shared/sources/xhr-response-source";
 import { QueryStringContext } from "../../../shared/types";
+import bigcommerceDataSource from "../../../shared/environment-data-sources/bigcommerce"
 
 const bigcommerceTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
-    xhrResponseSource((xhr) => {
-        try {
-            const transaction = JSON.parse(JSON.stringify(JSON.parse(xhr.responseText)));
-            const products = transaction?.lineItems?.physicalItems;
-            if (transaction.hasOwnProperty("orderId")) {
-                window.tracker("setUserId", transaction.billingAddress.email.toString());
-                window.tracker(
-                    "addTrans",
-                    transaction.orderId,
-                    retailId ?? appId,
-                    transaction.orderAmount,
-                    transaction.taxTotal || 0,
-                    transaction.shippingCostTotal || 0,
-                    transaction.billingAddress.city || "N/A",
-                    transaction.billingAddress.stateOrProvinceCode || "N/A",
-                    transaction.billingAddress.countryCode || "USA",
-                    transaction.currency.code,
-                );
-                products.forEach((item, index) => {
-                    window.tracker(
-                        "addItem",
-                        item.productId,
-                        item.sku,
-                        item.name,
-                        "N/A",
-                        item.listPrice,
-                        item.quantity,
-                        transaction.currency.code,
-                    );
-                });
-                window.tracker("trackTrans");
-            }
-        } catch (e) {
-        }
+    bigcommerceDataSource({
+        transactionEvent(transactionData) {
+            window.tracker(
+              "addTrans",
+              transactionData.id,
+              retailId ?? appId,
+              transactionData.total,
+              transactionData.tax,
+              transactionData.shipping,
+              transactionData.city,
+              transactionData.state,
+              transactionData.country
+            );
+      
+            transactionData.items.forEach((item) => {
+              window.tracker(
+                "addItem",
+                transactionData.id,
+                item.sku,
+                item.name,
+                item.category,
+                item.unitPrice,
+                item.quantity,
+                transactionData.currency
+              );
+            });
+            window.tracker("trackTrans");
+          },
     });
 };
 
