580654223c83e80052d6bd722f4a8eac3f465d91
Kimoy Catayas <elishakimcatayas@gmail.com>
2023-06-28 23:08:38 +0800

    dutchie update

--- src/shared/environment-data-sources/dutchie-plus.ts
+++ src/shared/environment-data-sources/dutchie-plus.ts
@@ -1,21 +1,32 @@
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
+  // important note: DUTCHIE-PLUS for curaleaf ONLY!!!
   window.dataLayer = window.dataLayer || [];
 
+   // if window.locatoin.href includes 'order-confirmation'
+   if (!window.location.href.includes('order-confirmation')){
+    return;
+  }
+
+  const orderContent: HTMLDivElement | any = document.getElementsByClassName('order-content')[0];
+  const text: string = orderContent?.innerText
+  const regex = /#(\d+)/;
+  const orderNumber = text.match(regex)[1];
+
+  if (localStorage.getItem('orderNumber') !== orderNumber) {
+    localStorage.setItem('orderNumber', orderNumber);
+    console.log(orderNumber);
+  }
+
   // loop through the dataLayer and find the purchase event
-  // then send the transactionEvent to the callback
-
-  // if local storage is empty then send transactionEvent
-  // if local storage: order id and current order id not the same then send transactionEvent
-  // if window.locatoin.href includes 'order-confirmation'
-  // important note: DUTCHIE-PLUS for curaleaf ONLY!!!
-
   for (let i = 0; i < window.dataLayer.length; i++) {
     const data = window.dataLayer[i];
     console.log("logging dataLayer event:");
     console.log(data);
-    if (data.event === "purchase") {
+    
+    // if local storage: order id and current order id not the same then send transactionEvent
+    if (data.event === "purchase" && localStorage.getItem('orderNumber') !== orderNumber) {
       const { id, revenue, tax } = data.ecommerce.purchase.actionField;
       const items = data.ecommerce.purchase.products;
 
