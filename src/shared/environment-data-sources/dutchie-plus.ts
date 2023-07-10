4130df86189de92b5d841543c8b293498467bde1
Kimoy Catayas <elishakimcatayas@gmail.com>
2023-07-11 05:20:58 +0800

    remove console logs

--- src/shared/environment-data-sources/dutchie-plus.ts
+++ src/shared/environment-data-sources/dutchie-plus.ts
@@ -15,17 +15,10 @@ const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>)
 
     // if local storage: order id and current order id not the same then send transactionEvent
     if (data.event === "purchase") {
-      console.log("logging dataLayer event:");
-      console.log(data);
-
       const orderContent: HTMLDivElement | any = document.getElementsByClassName("order-content")[0];
       const text: string = orderContent?.innerText;
       const regex = /#(\d+)/;
       const orderNumber = text.match(regex)[1];
-
-      console.log("orderNumber: ", orderNumber);
-
-      console.log(localStorage.getItem("orderNumber") === orderNumber);
 
       if (localStorage.getItem("orderNumber") === orderNumber) {
         return;
@@ -34,34 +27,8 @@ const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>)
       // set order id to local storage to prevent duplicate events
       localStorage.setItem("orderNumber", orderNumber);
 
-      console.log("tracking order...");
-
       const { id, revenue, tax } = data.ecommerce.purchase.actionField;
       const items = data.ecommerce.purchase.products;
-
-      console.log({
-        total: parseFloat(revenue),
-        id: id.toString(),
-        tax: parseFloat(tax || 0),
-        shipping: 0,
-        city: "N/A",
-        state: "N/A",
-        country: "N/A",
-        currency: "USD",
-        items: items.map((item) => {
-          const { id, name, category, price, quantity } = item;
-
-          return {
-            orderId: id.toString(),
-            sku: id.toString(),
-            name: name?.toString() || "N/A",
-            category: category?.toString() || "N/A",
-            unitPrice: parseFloat(price || 0),
-            quantity: parseInt(quantity || 1),
-            currency: "USD",
-          } as TransactionCartItem;
-        }),
-      });
 
       transactionEvent({
         total: parseFloat(revenue),
