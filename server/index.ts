bd20c25a41e5d78e4d7d2c7b74f3bee1b67d2618
Kimoy Catayas <elishakimcatayas@gmail.com>
2023-08-10 05:05:14 +0800

    WIP

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static("dist"));
 
--- src/v1/snowplow/events/create-tracker.ts
+++ src/v1/snowplow/events/create-tracker.ts
@@ -1,6 +1,6 @@
 import { QueryStringContext } from "../../../shared/types";
 
-const createTracker = ({ appId, collector, event }: QueryStringContext): void => {
+const createTracker = async ({ appId, collector, event }: QueryStringContext) => {
   // Loading tracker with the snowplow tag by fetching our sp.js file
   // Creates a global function called "tracker" which we use to access the Snowplow Tracker
   (function (e, o, n, t, a, c, i) {
@@ -19,10 +19,42 @@ const createTracker = ({ appId, collector, event }: QueryStringContext): void =>
     }
   })(window, document, "script", "//azsx401.dmp.cnna.io/sprite.js", "tracker");
 
+  // make a GET request to equative to get the user id
+  try {
+    const response = await fetch(
+      `https://sync.smartadserver.com/getuid?url=https%3A%2F%2F7a97-143-44-192-184.ngrok-free.app%2Fusersync%3Fuid%3D%5Bsas_uid%5D`,
+      {
+        method: "GET",
+        redirect: "follow",
+        headers: {
+          accept: 'application/json',
+        },
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+
+      // Create a context entity and add it to global context
+      let contextEntity = {
+        schema: "iglu:com.acme/user_context/jsonschema/1-0-0",
+        data,
+      };
+      console.log(contextEntity);
+      window.snowplow("addGlobalContexts", [contextEntity]);
+    } else {
+      console.error("Request failed with status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+
   // Creates the tracker with the appId and sends events to collector url
   window.tracker("newTracker", "cnna", `${collector}`, {
     appId: appId,
-    postPath: '/analytics/track',
+    postPath: "/analytics/track",
     discoverRootDomain: true,
     stateStorageStrategy: "cookieAndLocalStorage",
     cookieSameSite: "Lax",
