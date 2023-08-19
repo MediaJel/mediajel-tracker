29a3fbe80d52dd984b6769defdfe61d6a4bf4ee5
jonmediajel <jon@mediajel.com>
2023-08-19 11:46:13 +0800

    try fix

--- src/v2/imports/carts/bigcommerce.ts
+++ src/v2/imports/carts/bigcommerce.ts
@@ -8,32 +8,36 @@ const bigcommerceTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId
                 const transaction = JSON.parse(JSON.stringify(JSON.parse(xhr.responseText)));
                 if (transaction.hasOwnProperty("orderId")) {
                     if (transaction.hasOwnProperty("isComplete")) {
-                        const products = transaction?.lineItems?.physicalItems;
-                        window.tracker("setUserId", transaction.billingAddress.email.toString());
-                        window.tracker("addTrans", {
-                            id: transaction.orderId.toString(),
-                            affiliation: retailId ?? appId,
-                            total: parseFloat(transaction.orderAmount),
-                            tax: parseFloat(transaction.taxTotal) || 0,
-                            shipping: parseFloat(transaction.shippingCostTotal) || 0,
-                            city: (transaction.billingAddress.city || "N/A").toString(),
-                            state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
-                            country: (transaction.billingAddress.countryCode || "N/A").toString(),
-                            currency: (transaction.currency.code || "USD").toString(),
-                        });
-                        products.forEach((item, index) => {
-                            window.tracker("addItem", {
-                                orderId: transaction.orderId.toString(),
-                                productId: item.productId.toString(),
-                                sku: item.sku.toString(),
-                                name: (item.name || "N/A").toString(),
-                                category: "N/A",
-                                unitPrice: parseFloat(item.listPrice || 0),
-                                quantity: parseInt(item.quantity || 1),
+                        let latestOrder = null;
+                        if (latestOrder !== transaction.orderId.toString()) {
+                            const products = transaction?.lineItems?.physicalItems;
+                            window.tracker("setUserId", transaction.billingAddress.email.toString());
+                            window.tracker("addTrans", {
+                                id: transaction.orderId.toString(),
+                                affiliation: retailId ?? appId,
+                                total: parseFloat(transaction.orderAmount),
+                                tax: parseFloat(transaction.taxTotal) || 0,
+                                shipping: parseFloat(transaction.shippingCostTotal) || 0,
+                                city: (transaction.billingAddress.city || "N/A").toString(),
+                                state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
+                                country: (transaction.billingAddress.countryCode || "N/A").toString(),
                                 currency: (transaction.currency.code || "USD").toString(),
                             });
-                        });
-                        window.tracker("trackTrans");
+                            products.forEach((item, index) => {
+                                window.tracker("addItem", {
+                                    orderId: transaction.orderId.toString(),
+                                    productId: item.productId.toString(),
+                                    sku: item.sku.toString(),
+                                    name: (item.name || "N/A").toString(),
+                                    category: "N/A",
+                                    unitPrice: parseFloat(item.listPrice || 0),
+                                    quantity: parseInt(item.quantity || 1),
+                                    currency: (transaction.currency.code || "USD").toString(),
+                                });
+                            });
+                            window.tracker("trackTrans");
+                            latestOrder.push(transaction.orderId.toString())
+                        }
                     }
                 }
             }
