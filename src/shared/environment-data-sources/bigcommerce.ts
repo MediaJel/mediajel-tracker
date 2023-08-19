e380552f80c1cfff0cc063984cddcd6d6f44b5d4
jonmediajel <jon@mediajel.com>
2023-08-19 12:49:37 +0800

    try fix bigcommerce

--- src/shared/environment-data-sources/bigcommerce.ts
+++ src/shared/environment-data-sources/bigcommerce.ts
@@ -9,29 +9,33 @@ const bigcommerceDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>)
                 const products = transaction?.lineItems?.physicalItems;
                 if (transaction.hasOwnProperty("orderId")) {
                     if (transaction.hasOwnProperty("isComplete")) {
-                        transactionEvent({
-                            id: transaction.orderId.toString(),
-                            total: parseFloat(transaction.orderAmount),
-                            tax: parseFloat(transaction.taxTotal) || 0,
-                            shipping: parseFloat(transaction.shippingCostTotal) || 0,
-                            city: (transaction.billingAddress.city || "N/A").toString(),
-                            state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
-                            country: (transaction.billingAddress.countryCode || "N/A").toString(),
-                            currency: (transaction.currency.code || "USD").toString(),
-                            items: products.map((product) => {
-                                const { productId, sku, name, listPrice, quantity } = product;
-                                return {
-                                    orderId: transaction.orderId.toString(),
-                                    productId: productId.toString(),
-                                    sku: sku.toString(),
-                                    name: (name || "N/A").toString(),
-                                    category: "N/A",
-                                    unitPrice: parseFloat(listPrice || 0),
-                                    quantity: parseInt(quantity || 1),
-                                    currency: (transaction.currency.code || "USD").toString(),
-                                } as TransactionCartItem;
-                            }),
-                        });
+                        let latestOrder = null;
+                        if (latestOrder !== transaction.orderId.toString()) {
+                            transactionEvent({
+                                id: transaction.orderId.toString(),
+                                total: parseFloat(transaction.orderAmount),
+                                tax: parseFloat(transaction.taxTotal) || 0,
+                                shipping: parseFloat(transaction.shippingCostTotal) || 0,
+                                city: (transaction.billingAddress.city || "N/A").toString(),
+                                state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
+                                country: (transaction.billingAddress.countryCode || "N/A").toString(),
+                                currency: (transaction.currency.code || "USD").toString(),
+                                items: products.map((product) => {
+                                    const { productId, sku, name, listPrice, quantity } = product;
+                                    return {
+                                        orderId: transaction.orderId.toString(),
+                                        productId: productId.toString(),
+                                        sku: sku.toString(),
+                                        name: (name || "N/A").toString(),
+                                        category: "N/A",
+                                        unitPrice: parseFloat(listPrice || 0),
+                                        quantity: parseInt(quantity || 1),
+                                        currency: (transaction.currency.code || "USD").toString(),
+                                    } as TransactionCartItem;
+                                }),
+                            });
+                            latestOrder.push(transaction.orderId.toString())
+                        }
                     }
                 }
             }
