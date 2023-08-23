1bfffaeabbce7419cdb2488fd92d37dbc83b73c5
jonmediajel <jon@mediajel.com>
2023-08-23 12:56:12 +0800

    fix bigcommerce prod

--- src/v2/imports/carts/bigcommerce.ts
+++ src/v2/imports/carts/bigcommerce.ts
@@ -4,41 +4,38 @@ import { QueryStringContext } from "../../../shared/types";
 const bigcommerceTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
     xhrResponseSource((xhr) => {
         try {
-            if (window.location.pathname.includes('/checkout')) {
-                const transaction = JSON.parse(JSON.stringify(JSON.parse(xhr.responseText)));
-                const products = transaction?.lineItems?.physicalItems;
-                let latestOrder = null;
-                if (transaction.hasOwnProperty("orderId")) {
-                    if (transaction.hasOwnProperty("isComplete")) {
-                        if (transaction.isComplete === true) {
-                            if (latestOrder !== transaction.orderId.toString()) {
-                                window.tracker("setUserId", transaction.billingAddress.email.toString());
-                                window.tracker("addTrans", {
-                                    id: transaction.orderId.toString(),
-                                    affiliation: retailId ?? appId,
-                                    total: parseFloat(transaction.orderAmount),
-                                    tax: parseFloat(transaction.taxTotal) || 0,
-                                    shipping: parseFloat(transaction.shippingCostTotal) || 0,
-                                    city: (transaction.billingAddress.city || "N/A").toString(),
-                                    state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
-                                    country: (transaction.billingAddress.countryCode || "N/A").toString(),
-                                    currency: (transaction.currency.code || "USD").toString(),
+            const transaction = JSON.parse(JSON.stringify(JSON.parse(xhr.responseText)));
+            const products = transaction?.lineItems?.physicalItems;
+            let latestOrder = null;
+            if (transaction.hasOwnProperty("orderId")) {
+                if (transaction.hasOwnProperty("status")) {
+                    if (transaction.status === "INCOMPLETE") {
+                        if (latestOrder !== transaction.orderId.toString()) {
+                            window.tracker("addTrans", {
+                                id: transaction.orderId.toString(),
+                                affiliation:  retailId ?? appId,
+                                total: parseFloat(transaction.orderAmount),
+                                tax: parseFloat(transaction.taxTotal) || 0,
+                                shipping: parseFloat(transaction.shippingCostTotal) || 0,
+                                city: (transaction.billingAddress.city || "N/A").toString(),
+                                state: (transaction.billingAddress.stateOrProvinceCode || "N/A").toString(),
+                                country: (transaction.billingAddress.countryCode || "N/A").toString(),
+                                currency: "USD",
+                            });
+                            products.forEach((item, index) => {
+                                window.tracker("addItem", {
+                                    orderId: transaction.orderId.toString(),
+                                    productId: item.productId.toString(),
+                                    sku: item.sku.toString(),
+                                    name: (item.name || "N/A").toString(),
+                                    category: "N/A",
+                                    unitPrice: parseFloat(item.listPrice || 0),
+                                    quantity: parseInt(item.quantity || 1),
+                                    currency: "USD",
                                 });
-                                products.forEach((item, index) => {
-                                    window.tracker("addItem", {
-                                        orderId: transaction.orderId.toString(),
-                                        productId: item.productId.toString(),
-                                        sku: item.sku.toString(),
-                                        name: (item.name || "N/A").toString(),
-                                        category: "N/A",
-                                        unitPrice: parseFloat(item.listPrice || 0),
-                                        quantity: parseInt(item.quantity || 1),
-                                        currency: (transaction.currency.code || "USD").toString(),
-                                    });
-                                });
-                                window.tracker("trackTrans");
-                                latestOrder.push(transaction.orderId.toString())
-                            }
+                            });
+                            window.tracker("trackTrans");
+                            latestOrder.push(transaction.orderId.toString())
                         }
                     }
                 }
