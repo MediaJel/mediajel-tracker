91c13b17d1a4503b369491bc3206cf2832c44185
pacholoamit <pacholoamit.tech@gmail.com>
2023-08-29 01:46:48 +0800

    change url of sp.js

--- src/v1/snowplow/events/create-tracker.ts
+++ src/v1/snowplow/events/create-tracker.ts
@@ -17,12 +17,12 @@ const createTracker = ({ appId, collector, event }: QueryStringContext): void =>
       c.src = t;
       i.parentNode.insertBefore(c, i);
     }
-  })(window, document, "script", "//cmpgn.cmpgntrckr.com/api.js", "tracker");
+  })(window, document, "script", "//dm2q9qfzyjfox.cloudfront.net/sp.js", "tracker");
 
   // Creates the tracker with the appId and sends events to collector url
   window.tracker("newTracker", "cnna", `${collector}`, {
     appId: appId,
-    postPath: '/analytics/track',
+    postPath: "/analytics/track",
     discoverRootDomain: true,
     stateStorageStrategy: "cookieAndLocalStorage",
     cookieSameSite: "Lax",
