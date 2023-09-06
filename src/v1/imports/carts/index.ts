8a7ea651cac5b1cb30bc025d912b89e5b0e7d15d
Kimoy Catayas <elishakimcatayas@gmail.com>
2023-09-07 00:03:39 +0800

    Refactor for improvements

--- src/v1/imports/carts/index.ts
+++ src/v1/imports/carts/index.ts
@@ -1,92 +1,72 @@
 import { QueryStringContext } from "../../../shared/types";
 
-export default async (context: QueryStringContext): Promise<void> => {
-  const { appId, environment, retailId } = context;
+const handleStableEnvironment = (environment: string, appId: string, retailId: string): void => {
   switch (environment) {
-    //* STABLE
     case "jane":
-      import("./jane").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "dutchie-subdomain":
-      import("./dutchie-subdomain").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "dutchie-iframe":
-      import("./dutchie-iframe").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "meadow":
-      import("./meadow").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "tymber":
-      import("./tymber").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "woocommerce":
-      import("./woocommerce").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "greenrush":
-      import("./greenrush").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "buddi":
-      import("./buddi").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "shopify":
-      import("./shopify").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* UNSTABLE
-    case "lightspeed":
-      import("./lightspeed").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "olla":
-      import("./olla").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "grassdoor":
-      import("./grassdoor").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "wefunder":
-      import("./wefunder").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* UNSTABLE
-    case "ecwid":
-      import("./ecwid").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* UNSTABLE
-    case "square":
-      import("./square").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* UNSTABLE
-    case "dutchieplus":
-      import("./dutchie-plus").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* UNSTABLE
-    case "webjoint":
-      import("./webjoint").then(({ default: load }): void => load({ appId, retailId }));
-    //* UNSTABLE
-    case "sticky-leaf":
-      import("./sticky-leaf").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "dispense":
-      import("./dispense").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "bigcommerce":
-      import("./bigcommerce").then(({ default: load }): void => load({ appId, retailId }));
-      break;
-    //* STABLE
     case "yotpo":
-      import("./yotpo").then(({ default: load }): void => load({ appId, retailId }));
+      import(`./${environment.replace("-", "_")}`).then(({ default: load }): void => load({ appId, retailId }));
       break;
     default:
-      throw new Error("Undefined environment");
+      throw new Error(`Undefined stable environment: ${environment}`);
+  }
+};
+
+const handleUnstableEnvironment = (environment: string, appId: string, retailId: string): void => {
+  switch (environment) {
+    case "lightspeed":
+    case "ecwid":
+    case "square":
+    case "dutchieplus":
+    case "webjoint":
+    case "sticky-leaf":
+      import(`./${environment.replace("-", "_")}`).then(({ default: load }): void => load({ appId, retailId }));
+      break;
+    default:
+      throw new Error(`Undefined unstable environment: ${environment}`);
+  }
+};
+
+export default async (context: QueryStringContext): Promise<void> => {
+  const { appId, environment, retailId } = context;
+
+  const stableEnvironments: string[] = [
+    "jane",
+    "dutchie-subdomain",
+    "dutchie-iframe",
+    "meadow",
+    "tymber",
+    "woocommerce",
+    "greenrush",
+    "buddi",
+    "shopify",
+    "olla",
+    "grassdoor",
+    "wefunder",
+    "dispense",
+    "bigcommerce",
+    "yotpo",
+  ];
+
+  const unstableEnvironments: string[] = ["lightspeed", "ecwid", "square", "dutchieplus", "webjoint", "sticky-leaf"];
+
+  if (stableEnvironments.includes(environment)) {
+    handleStableEnvironment(environment, appId, retailId);
+  } else if (unstableEnvironments.includes(environment)) {
+    handleUnstableEnvironment(environment, appId, retailId);
+  } else {
+    throw new Error("Undefined environment");
   }
 };
