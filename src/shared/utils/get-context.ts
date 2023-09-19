1d3149d650bdb2a1722a046d6350d340560b55e3
Jon Mateo <76018662+jonmediajel@users.noreply.github.com>
2023-09-19 16:59:40 +0800

    Revert "[QA PASSED] [HOTFIX] Fix multiple tags"

--- src/shared/utils/get-context.ts
+++ src/shared/utils/get-context.ts
@@ -1,31 +1,21 @@
 import { QueryStringContext, QueryStringParams } from "../types";
 
-const getContext = () => {
-  const scriptContents: QueryStringContext[] = [];
-  const scriptElements = document.querySelectorAll("script");
+// Locates our tag
+const getContext = (): QueryStringContext => {
+  const scripts: HTMLCollectionOf<HTMLScriptElement> = document.getElementsByTagName("script");
+  const target: HTMLScriptElement = (document.currentScript as HTMLScriptElement) || scripts[scripts.length - 1];
+  const substring: string = target.src.substring(target.src.indexOf("?"));
+  const urlSearchParams: URLSearchParams = new URLSearchParams(substring);
+  const { mediajelAppId, appId, version, ...params } = Object.fromEntries(
+    urlSearchParams.entries()
+  ) as unknown as QueryStringParams;
 
-  scriptElements.forEach((scriptElement) => {
-    const srcAttributeValue = scriptElement.getAttribute("src");
-
-    if (srcAttributeValue && srcAttributeValue.includes("appId=")) {
-      const substring: string = srcAttributeValue.split("?")[1];
-      const urlSearchParams: URLSearchParams = new URLSearchParams(substring);
-      const { mediajelAppId, appId, version, ...params } = Object.fromEntries(
-        urlSearchParams.entries()
-      ) as unknown as QueryStringParams;
-
-      const context = {
-        appId: appId || mediajelAppId,
-        version: version || "1",
-        collector: params.test ? process.env.MJ_STAGING_COLLECTOR_URL : process.env.MJ_PRODUCTION_COLLECTOR_URL,
-        ...params,
-      } as QueryStringContext;
-
-      scriptContents.push(context);
-    }
-  });
-
-  return scriptContents;
+  return {
+    appId: appId || mediajelAppId, // Legacy support for old universal tag
+    version: version || "1",
+    collector: params.test ? process.env.MJ_STAGING_COLLECTOR_URL : process.env.MJ_PRODUCTION_COLLECTOR_URL,
+    ...params,
+  };
 };
 
 export default getContext;
