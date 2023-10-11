2e452be19e1a322c29a69ac85f2592b2cf73d1fc
pacholoamit <pacholoamit.tech@gmail.com>
2023-10-12 02:28:39 +0800

    chore: remove unnecessary code and console.log statements

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,40 @@ import getContext from "./shared/utils/get-context";
 import { QueryStringContext } from "./shared/types";
 
 (async (): Promise<void> => {
-
   try {
-
     document.addEventListener("DOMContentLoaded", () => {
       const context = getContext();
 
-      console.log('context', context);
+      console.log("context", context);
 
       const uniqueContext: QueryStringContext[] = [];
       const seenCombinations: Set<string> = new Set();
 
       for (const obj of context) {
         const combination = `${obj.appId}-${obj.version}-${obj.collector}-${obj.environment}`;
-    
+
         if (!seenCombinations.has(combination)) {
-            seenCombinations.add(combination);
-            uniqueContext.push(obj);
+          seenCombinations.add(combination);
+          uniqueContext.push(obj);
         }
       }
-    
-      console.log('uniqueContext', uniqueContext);
 
-      uniqueContext.forEach((ctxt: QueryStringContext)  => {
+      console.log("uniqueContext", uniqueContext);
+
+      uniqueContext.forEach((ctxt: QueryStringContext) => {
         console.log("MJ Tag Context", ctxt);
-  
+
         // Load plugin
         if (ctxt.plugin) {
           import("./plugins").then(({ default: load }): void => load(ctxt));
         }
-  
+
         // Return early if the appId is not specified
         if (ctxt.plugin && !ctxt.appId) return;
-  
+
         // Validations
         if (!ctxt.appId) throw new Error("appId is required");
-  
+
         switch (ctxt.version) {
           case "1":
             import("./v1").then(({ default: load }) => load(ctxt));
@@ -48,7 +46,6 @@ import { QueryStringContext } from "./shared/types";
         }
       });
     });
-
   } catch (err) {
     const clientError = `An error has occured, please contact your pixel provider: `;
     console.error(clientError + err.message);
