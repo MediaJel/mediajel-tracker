110d6ee1dde12a35dbe433c90d210697ca869ce5
pacholoamit <pacholoamit.tech@gmail.com>
2023-10-12 02:39:08 +0800

    refactor: Update dutchiePlusDataSource function to handle dutchieplus cart transactions

--- src/shared/environment-data-sources/dutchie-plus.ts
+++ src/shared/environment-data-sources/dutchie-plus.ts
@@ -1,7 +1,7 @@
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
-  // IMPORTANT NOTE: dutchieplus cart CURALEAF is Paid search only & greenvalleydispensary is display 
+  // IMPORTANT NOTE: dutchieplus cart CURALEAF is Paid search only & greenvalleydispensary is display
   window.dataLayer = window.dataLayer || [];
 
   // if window.locatoin.href includes 'order-confirmation'
@@ -47,6 +47,35 @@ const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>)
             category: category?.toString() || "N/A",
             unitPrice: parseFloat(price || 0),
             quantity: parseInt(quantity || 1),
+            currency: "USD",
+          } as TransactionCartItem;
+        }),
+      });
+    }
+
+    if (data[1] === "purchase") {
+      console.log("Dutchie plus transaction detected");
+      console.log(data[2]);
+      const transactionData = data[2];
+      const items = transactionData.items;
+
+      transactionEvent({
+        total: parseFloat(transactionData.value),
+        id: transactionData.rawData.orderNumber,
+        tax: parseFloat(transactionData.tax || 0),
+        shipping: parseFloat(transactionData.shipping || 0),
+        city: transactionData.city || "N/A",
+        state: transactionData.state || "N/A",
+        country: "USA",
+        currency: "USD",
+        items: items.map((item) => {
+          return {
+            orderId: transactionData.rawData.orderNumber,
+            sku: item.item_id,
+            name: item.item_name || "N/A",
+            category: item.item_category || "N/A",
+            unitPrice: parseFloat(item.price || 0),
+            quantity: parseInt(item.quantity || 1),
             currency: "USD",
           } as TransactionCartItem;
         }),
