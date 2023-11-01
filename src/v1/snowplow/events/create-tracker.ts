e1962d3d58c04631528f780855a1812e594af5a0
pacholoamit <pacholoamit.tech@gmail.com>
2023-11-01 22:36:59 +0800

    set dynamic tracker namespace

--- src/v1/snowplow/events/create-tracker.ts
+++ src/v1/snowplow/events/create-tracker.ts
@@ -20,7 +20,7 @@ const createTracker = ({ appId, collector, event }: QueryStringContext): void =>
   })(window, document, "script", "//dm2q9qfzyjfox.cloudfront.net/sp.js", "tracker");
 
   // Creates the tracker with the appId and sends events to collector url
-  window.tracker("newTracker", "cnna", `${collector}`, {
+  window.tracker("newTracker", appId, `${collector}`, {
     appId: appId,
     postPath: "/analytics/track",
     discoverRootDomain: true,
