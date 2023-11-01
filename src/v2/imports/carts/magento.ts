fecc46d7ad1941a05663da082915119a768d84cf
jonmediajel <jon@mediajel.com>
2023-11-01 12:18:33 +0800

    fix magento

--- src/v2/imports/carts/magento.ts
+++ src/v2/imports/carts/magento.ts
@@ -1,59 +1,35 @@
-import { xhrResponseSource } from "../../../shared/sources/xhr-response-source";
+import magentoDataSource from "@/shared/environment-data-sources/magento";
 import { QueryStringContext } from "../../../shared/types";
 
 const magentoTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
-  xhrResponseSource((xhr) => {
-    try {
-      const getData = JSON.parse(xhr.responseText);
+  magentoDataSource({
+    transactionEvent(transactionData) {
+      window.tracker("addTrans", {
+        orderId: transactionData.id,
+        affiliation: retailId ?? appId,
+        total: transactionData.total,
+        tax: transactionData.tax,
+        shipping: transactionData.shipping,
+        city: transactionData.city,
+        state: transactionData.state,
+        country: transactionData.country,
+        currency: "USD",
+      });
 
-      const products = getData && getData.items;
-
-      if (products !== undefined || products.length !== 0) {
-        const dataToJSON = JSON.stringify(getData);
-        sessionStorage.setItem("pixelData", dataToJSON);
-      }
-    } catch (error) {}
+      transactionData.items.forEach((item) => {
+        window.tracker("addItem", {
+          orderId: transactionData.id,
+          sku: item.sku,
+          name: item.name,
+          category: item.category,
+          price: item.unitPrice,
+          quantity: item.quantity,
+          currency: "USD",
+        });
+      });
+      window.tracker("trackTrans");
+    },
   });
-  try {
-    if (window.location.pathname.includes("/checkout/onepage/success/")) {
-      setTimeout(() => {
-        const storedData = sessionStorage.getItem("pixelData");
-        const retrievedObject = JSON.parse(storedData);
-        const productsList = retrievedObject && retrievedObject.items;
-
-        const checkoutSuccessElement = document.querySelector(".checkout-success");
-        const orderNumberElement = checkoutSuccessElement.querySelector("span");
-        const orderNumber = orderNumberElement.textContent.trim();
-
-        window.tracker("addTrans", {
-          orderId: orderNumber,
-          affiliation: retailId ?? appId,
-          total: parseFloat(retrievedObject.base_grand_total),
-          tax: parseFloat(retrievedObject.base_tax_amount) || 0,
-          shipping: parseFloat(retrievedObject.base_shipping_amount) || 0,
-          city: "N/A",
-          state: "N/A",
-          country: "N/A",
-          currency: retrievedObject.quote_currency_code || "USD",
-        });
-
-        productsList.forEach((item) => {
-          window.tracker("addItem", {
-            orderId: orderNumber,
-            sku: item.item_id || "N/A",
-            name: (item.name || "N/A").toString(),
-            category: "N/A",
-            price: parseFloat(item.price || 0),
-            quantity: parseInt(item.qty || 1),
-            currency: retrievedObject.quote_currency_code || "USD",
-          });
-        });
-        window.tracker("trackTrans");
-
-        sessionStorage.setItem("pixelData", "0");
-      }, 1000);
-    }
-  } catch (error) {}
 };
 
 export default magentoTracker;
