498d042e606287d7252633fd4f873c716a9baf57
Jon <jon@mediajel.com>
2024-04-26 10:57:15 +0800

    fix shopify undefined window checkout

--- src/shared/environment-data-sources/shopify.ts
+++ src/shared/environment-data-sources/shopify.ts
@@ -1,40 +1,37 @@
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const shopifyDataSource = ({ transactionEvent }: Pick<EnvironmentEvents, "transactionEvent">) => {
-  if (!window.Shopify.checkout) {
-    return;
-  }
 
-  const transaction = window.Shopify.checkout ? window.Shopify.checkout : window?.transactionOrder;
-  const products = transaction.line_items ? transaction.line_items : window?.transactionItems;
-  const email = transaction.email || "N/A";
+  const transaction = window?.Shopify?.checkout ? window?.Shopify?.checkout : window?.transactionOrder;
+  const products = transaction?.line_items ? transaction?.line_items : window?.transactionItems;
+  const email = transaction?.email || "N/A";
   const orderNumber = document.getElementsByClassName("os-order-number")[0]["innerText"] || "";
 
   window.tracker("setUserId", email);
 
   // liquid_total_price is legacy support for old shopify integration
-  if(window.Shopify.checkout){
+  if(window?.Shopify?.checkout){
     transactionEvent({
       userId: email,
-      id: `${(transaction.liquid_order_name || transaction.order_id).toString()} ${orderNumber && `- ${orderNumber}`}`,
-      total: parseFloat(transaction.liquid_total_price || transaction.total_price),
-      tax: parseFloat(transaction.total_tax || 0),
-      shipping: parseFloat(transaction.shipping_rate.price || 0),
-      city: (transaction.billing_address.city || "N/A").toString(),
-      state: (transaction.billing_address.province || "N/A").toString(),
-      country: (transaction.billing_address.country || "N/A").toString(),
-      currency: (transaction.currency || "N/A").toString(),
+      id: `${(transaction?.liquid_order_name || transaction?.order_id).toString()} ${orderNumber && `- ${orderNumber}`}`,
+      total: parseFloat(transaction?.liquid_total_price || transaction.total_price),
+      tax: parseFloat(transaction?.total_tax || 0),
+      shipping: parseFloat(transaction?.shipping_rate?.price || 0),
+      city: (transaction?.billing_address.city || "N/A").toString(),
+      state: (transaction?.billing_address.province || "N/A").toString(),
+      country: (transaction?.billing_address.country || "N/A").toString(),
+      currency: (transaction?.currency || "N/A").toString(),
       items: products.map((product: any) => {
         const { id, product_id, title, variant_title, price, quantity } = product;
         return {
-          orderId: (transaction.liquid_order_name || transaction.order_id).toString(),
+          orderId: (transaction?.liquid_order_name || transaction.order_id).toString(),
           productId: (id || product_id).toString(),
           sku: (id || product_id).toString(),
           name: (title || "N/A").toString(),
           category: "N/A", // No Category Field for Shopify in transactionItems
           unitPrice: parseFloat(price || 0),
           quantity: parseInt(quantity || 1),
-          currency: (transaction.currency || "USD").toString(),
+          currency: (transaction?.currency || "USD").toString(),
         } as TransactionCartItem;
       }),
     });
@@ -53,11 +50,11 @@ const shopifyDataSource = ({ transactionEvent }: Pick<EnvironmentEvents, "transa
         return {
           orderId: transaction?.id.toString(),
           productId: product?.id.toString(),
-          sku: productItem.sku.toString(),
-          name: productItem.product.title.toString(),
+          sku: productItem?.sku.toString(),
+          name: productItem?.product.title.toString(),
           category: "N/A", // No Category Field for Shopify in transactionItems
-          unitPrice: parseFloat(productItem.price.amount) || 0,
-          quantity: parseInt(product.quantity) || 1,
+          unitPrice: parseFloat(productItem?.price?.amount) || 0,
+          quantity: parseInt(product?.quantity) || 1,
           currency: (transaction?.currencyCode || "USD").toString(),
         } as TransactionCartItem;
       }),
