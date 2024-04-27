3493785238332eaf22fe479fc75a15866585b709
Jon <jon@mediajel.com>
2024-04-27 12:43:24 +0800

    revert back shopify

--- src/shared/environment-data-sources/shopify.ts
+++ src/shared/environment-data-sources/shopify.ts
@@ -1,85 +1,18 @@
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const shopifyDataSource = ({ transactionEvent }: Pick<EnvironmentEvents, "transactionEvent">) => {
-<<<<<<< HEAD
-=======
   if (!window.Shopify.checkout) {
     return;
   }
+
   const transaction = window.Shopify.checkout;
   const products = transaction.line_items;
   const email = transaction.email || "N/A";
   const orderNumber = document.getElementsByClassName("os-order-number")[0]["innerText"] || "";
->>>>>>> parent of 40f5576... Merge pull request #369 from MediaJel/staging
 
-  if(window.transactionOrder === undefined){
-    if (window.Shopify?.checkout === undefined) {
-      return;
-    }
-  } else {
-    const transaction = window?.transactionOrder;
-      const products = window?.transactionItems;
-      transactionEvent({
-        id: transaction?.order?.id,
-        total: parseFloat(transaction?.totalPrice?.amount || 0),
-        tax: parseFloat(transaction?.totalTax?.amount || 0),
-        shipping: parseFloat(transaction?.shippingLine?.price?.amount || 0),
-        city: "N/A",
-        state: "N/A",
-        country: "N/A",
-        currency: (transaction?.currencyCode || "USD").toString(),
-        items: products?.map((product: any) => {
-          const productItem = product?.variant;
-          return {
-            orderId: transaction?.order?.id.toString(),
-            productId: product?.id.toString(),
-            sku: productItem.sku.toString(),
-            name: productItem.product.title.toString(),
-            category: "N/A", // No Category Field for Shopify in transactionItems
-            unitPrice: parseFloat(productItem.price.amount) || 0,
-            quantity: parseInt(product.quantity) || 1,
-            currency: (transaction?.currencyCode || "USD").toString(),
-          } as TransactionCartItem;
-        }),
-      });
-  }
+  window.tracker("setUserId", email);
 
   // liquid_total_price is legacy support for old shopify integration
-<<<<<<< HEAD
-  if (window.Shopify?.checkout) {
-    const transaction = window.Shopify.checkout;
-    const products = transaction.line_items;
-    const email = transaction.email || "N/A";
-    const orderNumber = document.getElementsByClassName("os-order-number")[0]["innerText"] || "";
-  
-    window.tracker("setUserId", email);
-    transactionEvent({
-      userId: email,
-      id: `${(transaction.liquid_order_name || transaction.order_id).toString()} ${orderNumber && `- ${orderNumber}`}`,
-      total: parseFloat(transaction.liquid_total_price || transaction.total_price),
-      tax: parseFloat(transaction.total_tax || 0),
-      shipping: parseFloat(transaction.shipping_rate.price || 0),
-      city: (transaction.billing_address.city || "N/A").toString(),
-      state: (transaction.billing_address.province || "N/A").toString(),
-      country: (transaction.billing_address.country || "N/A").toString(),
-      currency: (transaction.currency || "N/A").toString(),
-      items: products.map((product: any) => {
-        const { id, product_id, title, variant_title, price, quantity } = product;
-        return {
-          orderId: (transaction.liquid_order_name || transaction.order_id).toString(),
-          productId: (id || product_id).toString(),
-          sku: (id || product_id).toString(),
-          name: (title || "N/A").toString(),
-          category: "N/A", // No Category Field for Shopify in transactionItems
-          unitPrice: parseFloat(price || 0),
-          quantity: parseInt(quantity || 1),
-          currency: (transaction.currency || "USD").toString(),
-        } as TransactionCartItem;
-      }),
-    });
-  }
-
-=======
 
   transactionEvent({
     userId: email,
@@ -93,27 +26,18 @@ const shopifyDataSource = ({ transactionEvent }: Pick<EnvironmentEvents, "transa
     currency: (transaction.currency || "N/A").toString(),
     items: products.map((product: any) => {
       const { id, product_id, title, variant_title, price, quantity } = product;
-      return {} as TransactionCartItem;
+      return {
+        orderId: (transaction.liquid_order_name || transaction.order_id).toString(),
+        productId: (id || product_id).toString(),
+        sku: (id || product_id).toString(),
+        name: (title || "N/A").toString(),
+        category: "N/A", // No Category Field for Shopify in transactionItems
+        unitPrice: parseFloat(price || 0),
+        quantity: parseInt(quantity || 1),
+        currency: (transaction.currency || "USD").toString(),
+      } as TransactionCartItem;
     }),
   });
-
-  products.forEach((items) => {
-    const { id, product_id, title, variant_title, price, quantity } = items;
-
-    window.tracker(
-      "addItem",
-      (transaction.liquid_order_name || transaction.order_id).toString(),
-      (id || product_id).toString(),
-      (title || "N/A").toString(),
-      (variant_title || "N/A").toString(),
-      parseFloat(price || 0),
-      parseInt(quantity || 1),
-      (transaction.currency || "USD").toString()
-    );
-  });
-
-  window.tracker("trackTrans");
->>>>>>> parent of 40f5576... Merge pull request #369 from MediaJel/staging
 };
 
 export default shopifyDataSource;
