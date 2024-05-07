5c86b3455ddb90e9d3981cea55fe6f2d353ccf9a
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-08 04:09:26 +0800

    Revert "Revert "track errors v1""
    This reverts commit d84ff17945ce724852339f83e852532da8bff94c.

--- src/shared/environment-data-sources/webjoint.ts
+++ src/shared/environment-data-sources/webjoint.ts
@@ -3,31 +3,35 @@ import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const webjointDataSource = ({ transactionEvent }: Pick<EnvironmentEvents, "transactionEvent">) => {
   xhrRequestSource((data: any): void => {
-    const parsedData = JSON.parse(data);
+    try {
+      const parsedData = JSON.parse(data);
 
-    if (parsedData && Object.keys(parsedData).includes("orders")) {
-      transactionEvent({
-        id: parsedData.orders[0].id || "N/A",
-        total: parseFloat(parsedData.orders[0].total) || 0,
-        tax: parseFloat(parsedData.orders[0].taxes) || 0,
-        city: "N/A",
-        country: "USA",
-        currency: "USD",
-        shipping: 0,
-        state: "N/A",
-        items: parsedData.orders[0].details.map((item: any) => {
-          const { name, quantity } = item;
-          return {
-            orderId: parsedData.orders[0]["_id"].toString() || parsedData.orders[0].id.toString() || "N/A",
-            category: "N/A".toString(),
-            currency: "USD",
-            name: (name || "N/A").toString(),
-            quantity: parseFloat(quantity) || 1,
-            sku: "N/A",
-            unitPrice: 0,
-          } as TransactionCartItem;
-        }),
-      });
+      if (parsedData && Object.keys(parsedData).includes("orders")) {
+        transactionEvent({
+          id: parsedData.orders[0].id || "N/A",
+          total: parseFloat(parsedData.orders[0].total) || 0,
+          tax: parseFloat(parsedData.orders[0].taxes) || 0,
+          city: "N/A",
+          country: "USA",
+          currency: "USD",
+          shipping: 0,
+          state: "N/A",
+          items: parsedData.orders[0].details.map((item: any) => {
+            const { name, quantity } = item;
+            return {
+              orderId: parsedData.orders[0]["_id"].toString() || parsedData.orders[0].id.toString() || "N/A",
+              category: "N/A".toString(),
+              currency: "USD",
+              name: (name || "N/A").toString(),
+              quantity: parseFloat(quantity) || 1,
+              sku: "N/A",
+              unitPrice: 0,
+            } as TransactionCartItem;
+          }),
+        });
+      }
+    } catch (error) {
+      window.tracker("trackError", error, "WEBJOINT");
     }
   });
 };
