a5701bdda0596a5a6a10bdcec9b9650bbe976a8b
SeanRizarre <sean@mediajel.com>
2024-07-22 21:37:58 +0800

    fix: added another use case for dispense cart with fetchSource

--- src/shared/environment-data-sources/dispense.ts
+++ src/shared/environment-data-sources/dispense.ts
@@ -1,7 +1,11 @@
 import { datalayerSource } from "../sources/google-datalayer-source";
+import { fetchSource } from "../sources/fetch-source";
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const dispenseDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
+  // fetchSource is optional second param to handle dispense cart;
+  let success = false;
+
   datalayerSource((data) => {
     if (data[1] === "purchase") {
       try {
@@ -29,11 +33,59 @@ const dispenseDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) =>
             } as TransactionCartItem;
           }),
         });
+
+        success = true; // prevents fetchSource from triggering if datalayer is successful
       } catch (error) {
         // window.tracker('trackError', JSON.stringify(error), 'DISPENSE')
       }
     }
   }, window.gtmDataLayer); // special case for dispense; window.dataLayer is renamed to window.gtmDataLayer
+
+  //  Triggers if datalayer is not available
+
+  if (!success) {
+    fetchSource(
+      (request) => {},
+      (reponse, responseBody) => {
+        try {
+          const key = "key";
+
+          if (!localStorage.getItem(key)) {
+            if (window.location.href.includes("/checkout-complete")) {
+              window.tracker(
+                "addTrans",
+                responseBody.id,
+                "N/A",
+                parseFloat(responseBody.total || "0"),
+                parseFloat(responseBody.totalTax || "0"),
+                0,
+                "N/A",
+                "N/A",
+                "N/A",
+                "USD"
+              );
+
+              responseBody.items.forEach((item) => {
+                window.tracker(
+                  "addItem",
+                  responseBody.id,
+                  item.product.sku || "N/A",
+                  item.product.name || "N/A",
+                  item.product.productCategoryName || "N/A",
+                  parseFloat(item.price || "0"),
+                  parseInt(item.quantity || "0"),
+                  "USD"
+                );
+              });
+
+              window.tracker("trackTrans");
+            }
+            localStorage.setItem(key, "loaded");
+          }
+        } catch (error) {}
+      }
+    );
+  }
 };
 
 export default dispenseDataSource;
