f7b2f9a2dac9424a0f87b9f89c2a967b764d82e1
SeanRizarre <sean@mediajel.com>
2024-07-29 09:35:06 +0800

    fix: New implementation for handling duplication

--- src/shared/environment-data-sources/dispense.ts
+++ src/shared/environment-data-sources/dispense.ts
@@ -3,54 +3,70 @@ import { fetchSource } from "../sources/fetch-source";
 import { EnvironmentEvents, TransactionCartItem } from "../types";
 
 const dispenseDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
-  // fetchSource is optional second param to handle dispense cart;
   let success = false;
+  let counter = 1;
 
-  datalayerSource((data) => {
-    if (data[1] === "purchase") {
-      try {
-        const { transaction_id, tax, value, items } = data[2];
-        transactionEvent?.({
-          total: parseFloat(value),
-          id: transaction_id.toString(),
-          tax,
-          shipping: 0,
-          city: "N/A",
-          state: "N/A",
-          country: "USA",
-          currency: "USD",
-          items: items.map((item) => {
-            const { item_name, item_category, price, quantity } = item;
+  const dataLayerCheck = () => {
+    datalayerSource((data) => {
+      if (data[1] === "purchase") {
+        try {
+          const { transaction_id, tax, value, items } = data[2];
+          transactionEvent?.({
+            total: parseFloat(value),
+            id: transaction_id.toString(),
+            tax,
+            shipping: 0,
+            city: "N/A",
+            state: "N/A",
+            country: "USA",
+            currency: "USD",
+            items: items.map((item) => {
+              const { item_name, item_category, price, quantity } = item;
 
-            return {
-              orderId: transaction_id.toString(),
-              sku: item_name.toString() || "N/A",
-              name: item_name?.toString() || "N/A",
-              category: item_category?.toString() || "N/A",
-              unitPrice: parseFloat(price || 0),
-              quantity: parseInt(quantity || 1),
-              currency: "USD",
-            } as TransactionCartItem;
-          }),
-        });
+              return {
+                orderId: transaction_id.toString(),
+                sku: item_name.toString() || "N/A",
+                name: item_name?.toString() || "N/A",
+                category: item_category?.toString() || "N/A",
+                unitPrice: parseFloat(price || 0),
+                quantity: parseInt(quantity || 1),
+                currency: "USD",
+              } as TransactionCartItem;
+            }),
+          });
 
-        success = true; // prevents fetchSource from triggering if datalayer is successful
-      } catch (error) {
-        // window.tracker('trackError', JSON.stringify(error), 'DISPENSE')
+          success = true; // Allows the IF case to execute break on the switch case
+        } catch (error) {
+          // window.tracker('trackError', JSON.stringify(error), 'DISPENSE')
+        }
+      }
+    }, window.gtmDataLayer); // special case for dispense; window.dataLayer is renamed to window.gtmDataLayer
+  };
+
+  // This function runs once per page load
+  const fetchSourceCheck = () => {
+    function runOncePerPageLoad(callback) {
+      const key = "key";
+
+      if (!sessionStorage.getItem(key)) {
+        console.log("First run in this session");
+        callback();
+        sessionStorage.setItem(key, "loaded");
+      } else {
+        console.log("Already run in this session");
       }
     }
-  }, window.gtmDataLayer); // special case for dispense; window.dataLayer is renamed to window.gtmDataLayer
 
-  //  Triggers if datalayer is not available
+    // Listen for the unload event to reset the sessionStorage item
+    window.addEventListener("beforeunload", () => {
+      sessionStorage.removeItem("key");
+    });
 
-  if (!success) {
     fetchSource(
       (request) => {},
       (reponse, responseBody) => {
         try {
-          const key = "key";
-
-          if (!localStorage.getItem(key)) {
+          runOncePerPageLoad(() => {
             if (window.location.href.includes("/checkout-complete")) {
               transactionEvent?.({
                 total: parseFloat(responseBody.total || 0),
@@ -74,11 +90,29 @@ const dispenseDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) =>
                 }),
               });
             }
-            localStorage.setItem(key, "loaded"); // do not remove, this prevents duplicating transactions
-          }
+          });
+
+          success = true; // Allows the IF case to execute break on the switch case
         } catch (error) {}
       }
     );
+  };
+
+  // ALLOWS FOR MULTIPLE USE CASES FOR SCALABILITY
+
+  switch (counter) {
+    case 1:
+      dataLayerCheck();
+      if (success) {
+        break;
+      }
+    case 2:
+      fetchSourceCheck();
+      if (success) {
+        break;
+      }
+    default:
+      console.log("DISPENSE ENVIRONMENT ERROR AT CASE ", counter);
   }
 };
 
