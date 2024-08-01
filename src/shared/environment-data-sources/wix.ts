571b36f33f26ea2f32dd33679cb7a1266b4439e9
Jon <jon@mediajel.com>
2024-08-01 08:33:46 +0800

    Wix adapter and fixes for some v2 carts

--- src/shared/environment-data-sources/wix.ts
+++ src/shared/environment-data-sources/wix.ts
@@ -1,17 +1,62 @@
+import { EnvironmentEvents, TransactionCartItem } from "../types";
+
 const wixTrackerDataSource = ({
     addToCartEvent,
     removeFromCartEvent,
     transactionEvent,
-}: {
-    addToCartEvent: (data: any) => void;
-    removeFromCartEvent: (data: any) => void;
-    transactionEvent: (data: any) => void;
-}) => {
+}: Partial<EnvironmentEvents>) => {
 
-    function registerListeners() {
+    function registerListeners(){
         window.wixDevelopersAnalytics.register('conversionListener', (e, p) => {
            console.log("e", e);
             console.log("p", p);
+
+            if(e === "AddToCart"){
+                addToCartEvent({
+                    sku: (p.sku || "N/A").toString(),
+                    name: (p.name || "N/A").toString(),
+                    category: (p.category || "N/A").toString(),
+                    unitPrice: parseFloat(p.price),
+                    quantity: parseFloat(p.quantity),
+                    currency: (p.currency || "USD").toString(),
+                });
+            }
+
+            if(e === "RemoveFromCart"){
+                removeFromCartEvent({
+                    sku: (p.sku || "N/A").toString(),
+                    name: (p.name || "N/A").toString(),
+                    category: (p.category || "N/A").toString(),
+                    unitPrice: parseFloat(p.price),
+                    quantity: parseFloat(p.quantity),
+                    currency: (p.currency || "USD").toString(),
+                });
+            }
+
+            if(e === "Purchase"){
+                transactionEvent({
+                    id: (p.orderId || "N/A").toString(),
+                    total: parseFloat(p.revenue || 0),
+                    tax: parseFloat(p.tax || 0),
+                    shipping: parseFloat(p.shipping || 0),
+                    city: "N/A",
+                    state: "N/A",
+                    country: "N/A",
+                    currency: (p.currency || "USD").toString(),
+                    items: p.contents.map((product) => {
+                        return {
+                            orderId: (p.orderId || "N/A").toString(),
+                            productId: (product.id || "N/A").toString(),
+                            sku: (product.sku || "N/A").toString(),
+                            name: (product.name || "N/A").toString(),
+                            category: (product.category || "N/A").toString(),
+                            unitPrice: parseFloat(product.price || 0),
+                            quantity: parseFloat(product.quantity || 1),
+                            currency: (p.currency || "USD").toString(),
+                        } as TransactionCartItem;
+                    })
+                });
+            }
         });
     }
 
@@ -19,29 +64,6 @@ const wixTrackerDataSource = ({
         registerListeners();
     });
 
-    window.wixDevelopersAnalytics.register('AddToCart', (e, p) => {
-        addToCartEvent({
-            sku: (p.sku || "N/A").toString(),
-            name: (p.name || "N/A").toString(),
-            category: (p.category || "N/A").toString(),
-            unitPrice: parseFloat(p.price),
-            quantity: parseFloat(p.quantity),
-            currency: (p.currency || "USD").toString(),
-        });
-    });
-
-    window.wixDevelopersAnalytics.register('RemoveFromCart', (e, p) => {
-        removeFromCartEvent({
-            sku: (p.sku || "N/A").toString(),
-            name: (p.name || "N/A").toString(),
-            category: (p.category || "N/A").toString(),
-            unitPrice: parseFloat(p.price),
-            quantity: parseFloat(p.quantity),
-            currency: (p.currency || "USD").toString(),
-        });
-    });
-
-    window.wixDevelopersAnalytics.register('Transaction', (e, p) => {
-        transactionEvent(p);
-    });
 };
+
+export default wixTrackerDataSource;
---
+++ src/v1/imports/carts/wix.ts
@@ -0,0 +1,61 @@
+import wixTrackerDataSource from "src/shared/environment-data-sources/wix";
+import { QueryStringContext } from "../../../shared/types";
+
+const wixTracker = ({ appId, retailId }: Pick<QueryStringContext, "appId" | "retailId">) => {
+  wixTrackerDataSource({
+    addToCartEvent(addToCartData) {
+      window.tracker(
+        "trackAddToCart",
+        addToCartData.sku,
+        addToCartData.name,
+        addToCartData.category,
+        addToCartData.unitPrice,
+        addToCartData.quantity,
+        addToCartData.currency
+      );
+    },
+
+    removeFromCartEvent(removeFromCartData) {
+      window.tracker(
+        "trackRemoveFromCart",
+        removeFromCartData.sku,
+        removeFromCartData.name,
+        removeFromCartData.category,
+        removeFromCartData.unitPrice,
+        removeFromCartData.quantity,
+        removeFromCartData.currency
+      );
+    },
+
+    transactionEvent(transactionData) {
+      window.tracker(
+        "addTrans",
+        transactionData.id,
+        retailId ?? appId,
+        transactionData.total,
+        transactionData.tax,
+        transactionData.shipping,
+        transactionData.city,
+        transactionData.state,
+        transactionData.country,
+        transactionData.currency
+      );
+
+      transactionData.items.forEach((item) => {
+        window.tracker(
+          "addItem",
+          item.orderId,
+          item.sku,
+          item.name,
+          item.category,
+          item.unitPrice,
+          item.quantity,
+          item.currency
+        );
+      });
+      window.tracker("trackTrans");
+    },
+  });
+};
+
+export default wixTracker;
