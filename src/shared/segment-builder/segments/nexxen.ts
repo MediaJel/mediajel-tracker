9967496cd530e25b92ae6e99cb72f2291a93ff66
pacholoamit <pacholoamit.tech@gmail.com>
2024-08-28 22:20:32 +0800

    fix: Remove unnecessary query parameters from pixel source URL

--- src/shared/segment-builder/segments/nexxen.ts
+++ src/shared/segment-builder/segments/nexxen.ts
@@ -8,7 +8,7 @@ const nexxenSegmentBuilder = (segmentId: string) => {
       console.log("Building s2 segment with segmentId: ", segmentId);
 
       const pixel = document.createElement("img");
-      pixel.src = `https://r.turn.com/r/beacon?b2=${segmentId}&cid=1234&bprice=50`;
+      pixel.src = `https://r.turn.com/r/beacon?b2=${segmentId}`;
       pixel.border = "0";
       document.body.appendChild(pixel);
     },
