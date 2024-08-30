8227b32290c8c9fc6088950438699c22086dee3d
pacholoamit <pacholoamit.tech@gmail.com>
2024-08-31 04:25:55 +0800

    style: Update color styles for log messages

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -11,6 +11,13 @@ export interface Logger {
   error: (...message: unknown[]) => void;
 }
 
+const colors = {
+  debug: "color: White; background: #3748B8; font-weight: bold; padding: 1px 4px; border-radius: 3px;",
+  info: "color: White; background: #388AB8; font-weight: bold; padding: 1px 4px; border-radius: 3px;",
+  warn: "color: White; background: #F26430; font-weight: bold; padding: 1px 4px; border-radius: 3px;",
+  error: "color: White; background: #E4572E; font-weight: bold; padding: 1px 4px; border-radius: 3px;",
+};
+
 const parseMsgToString = (message: unknown | unknown[]): string => {
   if (typeof message === "string" || typeof message === "number") {
     return message.toString();
@@ -24,13 +31,14 @@ const parseMsgToString = (message: unknown | unknown[]): string => {
 const formatMessage = (level: string, name: string, label: string | undefined, message: string): string => {
   const timestamp = new Date().toISOString();
   const identifier = label ? `${name} - ${label}` : name;
-  return `${timestamp} [${identifier}] ${level.toUpperCase()}: ${message}`;
+  return `${timestamp} [${identifier}] ${level.toUpperCase()} : ${message}`;
 };
 
 const createLogger = (name: string, options?: CreateLoggerOptionsInput): Logger => {
   const log = (level: string, ...message: unknown[]) => {
     const formattedMessage = formatMessage(level, name, options?.label, parseMsgToString(message));
-    console.log(formattedMessage);
+    const colorStyle = colors[level as keyof typeof colors] || "";
+    console.log(`%c${formattedMessage}`, colorStyle);
   };
 
   return {
@@ -41,6 +49,6 @@ const createLogger = (name: string, options?: CreateLoggerOptionsInput): Logger 
   };
 };
 
-const logger = createLogger("Cnna");
+const logger = createLogger("MJ");
 
 export default logger;
