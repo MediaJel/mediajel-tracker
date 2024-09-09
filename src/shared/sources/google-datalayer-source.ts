fd0842131990fc9eae9cf2d1d6fb1a201ad55cfd
Jon <jon@mediajel.com>
2024-09-09 16:56:14 +0800

    dataLayer source validation

--- src/shared/sources/google-datalayer-source.ts
+++ src/shared/sources/google-datalayer-source.ts
@@ -1,5 +1,9 @@
 export const datalayerSource = (callback: (data: any) => void, dataLayer: any = window.dataLayer || []): void => {
   // dataLayer is optional second param to handle dispense cart;
+  if (!dataLayer || typeof dataLayer.push !== 'function') {
+    console.error('dataLayer.push is not a function');
+    return;
+  }
   const originalPush = dataLayer.push;
   dataLayer.push = (...args: any): void => {
     originalPush(...args);
