2c29463ff1416136fd79d97ec652c098fc8f226a
Jon <jon@mediajel.com>
2024-09-12 17:48:18 +0800

    dutchie plus code update

--- src/shared/environment-data-sources/dutchie-plus.ts
+++ src/shared/environment-data-sources/dutchie-plus.ts
@@ -60,29 +60,31 @@ const dutchiePlusDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>)
       const getRevenue = data?.ecommerce?.purchase?.actionField?.revenue;
       const getTax = data?.ecommerce?.purchase?.actionField?.tax;
 
-      transactionEvent({
-        total: parseFloat(getRevenue),
-        id: getId,
-        tax: parseFloat(getTax || 0),
-        shipping: 0,
-        city: "N/A",
-        state: "N/A",
-        country: "N/A",
-        currency: "USD",
-        items: items.map((item) => {
-          const { id, name, category, price, quantity } = item;
+      if (getId) {
+        transactionEvent({
+          total: parseFloat(getRevenue),
+          id: getId.toString(),
+          tax: parseFloat(getTax || 0),
+          shipping: 0,
+          city: "N/A",
+          state: "N/A",
+          country: "N/A",
+          currency: "USD",
+          items: items.map((item) => {
+            const { id, name, category, price, quantity } = item;
 
-          return {
-            orderId: id.toString(),
-            sku: id.toString(),
-            name: name?.toString() || "N/A",
-            category: category?.toString() || "N/A",
-            unitPrice: parseFloat(price || 0),
-            quantity: parseInt(quantity || 1),
-            currency: "USD",
-          } as TransactionCartItem;
-        }),
-      });
+            return {
+              orderId: id.toString(),
+              sku: id.toString(),
+              name: name?.toString() || "N/A",
+              category: category?.toString() || "N/A",
+              unitPrice: parseFloat(price || 0),
+              quantity: parseInt(quantity || 1),
+              currency: "USD",
+            } as TransactionCartItem;
+          }),
+        });
+      }
     }
 
     if (data[1] === "purchase") {
