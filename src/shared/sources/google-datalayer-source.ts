498b560ad94755e427e744611997255c953c70d8
Jon <jon@mediajel.com>
2024-09-25 07:03:03 +0800

    hotfix datalayer

--- src/shared/sources/google-datalayer-source.ts
+++ src/shared/sources/google-datalayer-source.ts
@@ -1,10 +1,10 @@
 export const datalayerSource = (callback: (data: any) => void, dataLayer: any = window.dataLayer || []): void => {
   // dataLayer is optional second param to handle dispense cart;
-  if (!dataLayer || typeof dataLayer.push !== "function") {
-    console.error("dataLayer.push is not a function");
+  if (!dataLayer || typeof dataLayer.push !== 'function') {
+    console.error('dataLayer.push is not a function');
     return;
   }
-  const originalPush = dataLayer.push;
+  const originalPush = dataLayer.push.bind(dataLayer);;
   dataLayer.push = (...args: any): void => {
     originalPush(...args);
     callback(dataLayer.slice(-1)[0]); // Gets the newest array member of dataLayer
