48dbbfc0b57083b41a86c44945263fd768ff13eb
pacholoamit <pacholoamit.tech@gmail.com>
2024-09-24 23:36:04 +0800

    feat: Add new tracking script and version check

--- src/shared/utils/get-context.ts
+++ src/shared/utils/get-context.ts
@@ -1,6 +1,7 @@
 import { QueryStringContext, QueryStringParams } from "../types";
 
 // Locates our tag
+
 const getContext = (): QueryStringContext => {
   const scripts: HTMLCollectionOf<HTMLScriptElement> = document.getElementsByTagName("script");
   const target: HTMLScriptElement = (document.currentScript as HTMLScriptElement) || scripts[scripts.length - 1];
@@ -10,9 +11,18 @@ const getContext = (): QueryStringContext => {
     urlSearchParams.entries()
   ) as unknown as QueryStringParams;
 
+  const storedVersion = localStorage.getItem('version');
+
+  if (!storedVersion) {
+    localStorage.setItem('mj-tag-version', version || "1");
+  }
+
+  // Store the version in local storage
+  const resolvedVersion = storedVersion || localStorage.getItem('mj-tag-version');
+
   return {
     appId: appId || mediajelAppId, // Legacy support for old universal tag
-    version: version || "1", // tracker version
+    version: resolvedVersion, // tracker version
     collector: params.test ? process.env.MJ_STAGING_COLLECTOR_URL : process.env.MJ_PRODUCTION_COLLECTOR_URL,
     ...params,
   };
