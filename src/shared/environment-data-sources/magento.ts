4b9b4804183d35be67066e74bf7ae8039ddf3a32
ZeeLiger <zliger.a@gmail.com>
2024-10-23 08:06:57 +0800

    refactor: Improve sessionStorage handling in magentoDataSource

--- src/shared/environment-data-sources/magento.ts
+++ src/shared/environment-data-sources/magento.ts
@@ -4,61 +4,29 @@ import { datalayerSource } from '../sources/google-datalayer-source';
 import { xhrResponseSource } from '../sources/xhr-response-source';
 import { EnvironmentEvents, TransactionCartItem } from '../types';
 
-const magentoDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
-  xhrResponseSource((xhr) => {
-    try {
-      const getData = JSON.parse(xhr.responseText);
+const runOncePerPageLoad = (callback) => {
+  const key = "key";
 
-      const products = getData && getData.items;
-
-      if (products !== undefined || products.length !== 0) {
-        const dataToJSON = JSON.stringify(getData);
-        sessionStorage.setItem("pixelData", dataToJSON);
-      }
-    } catch (e) {}
-  });
-
-  if (window.location.pathname.includes("/checkout/onepage/success/")) {
-    setTimeout(() => {
-      try {
-        const storedData = sessionStorage.getItem("pixelData");
-        const retrievedObject = JSON.parse(storedData);
-        const productsList = retrievedObject && retrievedObject.items;
-
-        const checkoutSuccessElement = document.querySelector(".checkout-success");
-        const orderNumberElement = checkoutSuccessElement.querySelector("span");
-        const orderNumber = orderNumberElement.textContent.trim();
-
-        transactionEvent({
-          id: orderNumber,
-          total: parseFloat(retrievedObject.base_grand_total),
-          tax: parseFloat(retrievedObject.base_tax_amount) || 0,
-          shipping: parseFloat(retrievedObject.base_shipping_amount) || 0,
-          city: "N/A",
-          state: "N/A",
-          country: "N/A",
-          currency: "USD",
-          items: productsList.map((product) => {
-            const { item_id, name, price, qty } = product;
-            return {
-              orderId: orderNumber.toString(),
-              productId: item_id.toString(),
-              sku: item_id.toString(),
-              name: (name || "N/A").toString(),
-              category: "N/A",
-              unitPrice: parseFloat(price || 0),
-              quantity: parseInt(qty || 1),
-              currency: "USD",
-            } as TransactionCartItem;
-          }),
-        });
-      } catch (error) {
-        // window.tracker("trackError", JSON.stringify(error), "MAGENTO");
-      }
-      sessionStorage.setItem("pixelData", "0");
-    }, 1000);
+  if (!sessionStorage.getItem(key)) {
+    console.log("First run in this session");
+    callback();
+    sessionStorage.setItem(key, "loaded");
+  } else {
+    console.log("Already run in this session");
   }
+};
 
+// Listen for the unload event to reset the sessionStorage item
+window.addEventListener("beforeunload", () => {
+  sessionStorage.removeItem("key");
+});
+
+// Listen for page changes to reset the sessionStorage item
+window.addEventListener("popstate", () => {
+  sessionStorage.removeItem("key");
+});
+
+const magentoDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
   datalayerSource((data: any): void => {
     if(data.event === "purchase") {
       try {
@@ -85,8 +53,65 @@ const magentoDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => 
             } as TransactionCartItem;
           }),
         });
+        sessionStorage.setItem("key", "loaded");
       } catch (error) {
         // window.tracker("trackError", JSON.stringify(error), "MAGENTO");
+      }
+    }
+
+    if(!sessionStorage.getItem("key")) { 
+      xhrResponseSource((xhr) => {
+        try {
+          const getData = JSON.parse(xhr.responseText);
+
+          const products = getData && getData.items;
+
+          if (products !== undefined || products.length !== 0) {
+            const dataToJSON = JSON.stringify(getData);
+            sessionStorage.setItem("pixelData", dataToJSON);
+          }
+        } catch (e) {}
+      });
+
+      if (window.location.pathname.includes("/checkout/onepage/success/")) {
+        setTimeout(() => {
+          try {
+            const storedData = sessionStorage.getItem("pixelData");
+            const retrievedObject = JSON.parse(storedData);
+            const productsList = retrievedObject && retrievedObject.items;
+
+            const checkoutSuccessElement = document.querySelector(".checkout-success");
+            const orderNumberElement = checkoutSuccessElement.querySelector("span");
+            const orderNumber = orderNumberElement.textContent.trim();
+
+            transactionEvent({
+              id: orderNumber,
+              total: parseFloat(retrievedObject.base_grand_total),
+              tax: parseFloat(retrievedObject.base_tax_amount) || 0,
+              shipping: parseFloat(retrievedObject.base_shipping_amount) || 0,
+              city: "N/A",
+              state: "N/A",
+              country: "N/A",
+              currency: "USD",
+              items: productsList.map((product) => {
+                const { item_id, name, price, qty } = product;
+                return {
+                  orderId: orderNumber.toString(),
+                  productId: item_id.toString(),
+                  sku: item_id.toString(),
+                  name: (name || "N/A").toString(),
+                  category: "N/A",
+                  unitPrice: parseFloat(price || 0),
+                  quantity: parseInt(qty || 1),
+                  currency: "USD",
+                } as TransactionCartItem;
+              }),
+            });
+          } catch (error) {
+            // window.tracker("trackError", JSON.stringify(error), "MAGENTO");
+          }
+          sessionStorage.setItem("pixelData", "0");
+        }, 1000);
       }
     }
   });
