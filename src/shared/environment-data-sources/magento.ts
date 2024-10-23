31045e113f21c89cd7031b8c616f5ead0a3b2868
ZeeLiger <zliger.a@gmail.com>
2024-10-23 20:30:02 +0800

    feat: Add trans-deduplicator utility and sessionStorage usage

--- src/shared/environment-data-sources/magento.ts
+++ src/shared/environment-data-sources/magento.ts
@@ -3,6 +3,7 @@ import logger from 'src/shared/logger';
 import { xhrResponseSource } from '../sources/xhr-response-source';
 import { EnvironmentEvents, TransactionCartItem } from '../types';
 import { isTrackerLoaded } from '../sources/utils/is-tracker-loaded';
+import { runOncePerPageLoad } from '../sources/utils/trans-deduplicator';
 
 const magentoDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => {
   window.dataLayer = window.dataLayer || [];
@@ -14,28 +15,30 @@ const magentoDataSource = ({ transactionEvent }: Partial<EnvironmentEvents>) => 
       const ecommerce = data.ecommerce;
 
       isTrackerLoaded(() => { 
-        transactionEvent({
-          id: ecommerce.transaction_id.toString(),
-          total: parseFloat(ecommerce.value),
-          tax: parseFloat(ecommerce.tax) || 0,
-          shipping: parseFloat(ecommerce.shipping) || 0,
-          city: "N/A",
-          country: "USA",
-          currency: "USD",
-          state: "N/A",
-          items: ecommerce.items.map((item: any) => {
-            return {
-              orderId: ecommerce.transaction_id.toString(),
-              sku: item.id.toString(),
-              name: (item.name || "N/A").toString(),
-              category: (item.category || "N/A").toString(),
-              unitPrice: parseFloat(item.price || 0),
-              quantity: parseInt(item.quantity || 1),
-              currency: "USD",
-            } as TransactionCartItem;
-          }),
+        runOncePerPageLoad(() => {
+          transactionEvent({
+            id: ecommerce.transaction_id.toString(),
+            total: parseFloat(ecommerce.value),
+            tax: parseFloat(ecommerce.tax) || 0,
+            shipping: parseFloat(ecommerce.shipping) || 0,
+            city: "N/A",
+            country: "USA",
+            currency: "USD",
+            state: "N/A",
+            items: ecommerce.items.map((item: any) => {
+              return {
+                orderId: ecommerce.transaction_id.toString(),
+                sku: item.id.toString(),
+                name: (item.name || "N/A").toString(),
+                category: (item.category || "N/A").toString(),
+                unitPrice: parseFloat(item.price || 0),
+                quantity: parseInt(item.quantity || 1),
+                currency: "USD",
+              } as TransactionCartItem;
+            }),
+          });
+          sessionStorage.setItem("key", "loaded");
         });
-        sessionStorage.setItem("key", "loaded");
       });
     }
   }
