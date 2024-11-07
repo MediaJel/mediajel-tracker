55b2a2cc6375141d389c6d0b384110df30e4c92e
pacholoamit <pacholoamit.tech@gmail.com>
2024-11-08 00:14:45 +0800

    refactor: Remove unnecessary logs and improve duplicate transaction warning

--- src/shared/snowplow/extensions/deduplicator.ts
+++ src/shared/snowplow/extensions/deduplicator.ts
@@ -6,13 +6,10 @@ const withTransactionDeduplicationExtension = (snowplow: SnowplowTracker) => {
   const trackTransaction = snowplow.ecommerce.trackTransaction;
 
   snowplow.ecommerce.trackTransaction = (input) => {
-    logger.debug("Triggering Transaction Deduplication Extension");
     const transactionStorage = localStorage.getItem(snowplow.context.appId);
 
-    logger.debug("Transaction Storage", transactionStorage);
-
     if (transactionStorage === input.id) {
-      return logger.warn(`Transaction with id ${input.id} already tracked`);
+      return logger.warn(`Transaction with id ${input.id} already tracked, Disccarding duplicate transaction`);
     }
 
     trackTransaction(input);
