c40ac9b993f8471a7d8f86c91943f0b4af86f608
SeanRizarre <sean@mediajel.com>
2024-11-08 00:29:44 +0800

    Added logging on deduplicator

--- src/shared/snowplow/extensions/deduplicator.ts
+++ src/shared/snowplow/extensions/deduplicator.ts
@@ -5,14 +5,20 @@ export const withTransactionDeduplicationExtension = (snowplow: SnowplowTracker)
     const trackTransaction = snowplow.ecommerce.trackTransaction;
 
     snowplow.ecommerce.trackTransaction = (input) => {
+        localStorage.setItem(snowplow.context.appId, "");
         const transactionStorage = localStorage.getItem(snowplow.context.appId);
+        console.log("Verifying Transaction Count");
+        console.log("Storage Pre-Value: ", transactionStorage);
         
         if (transactionStorage === input.id) {
+            console.log("Cache detected duplicated transaction, skipping...");
             return;
         }
 
+        console.log("Transaction Verified, Initiating Tracking...");
         trackTransaction(input);
         localStorage.setItem(snowplow.context.appId, input.id);
+        console.log("Storage Post-Value: ", transactionStorage);
     }
     return snowplow;
 }
