b3f15f3db4aeb694f63a4786b408ad3df6240a41
Jon <jon@mediajel.com>
2024-12-03 13:56:41 +0800

    try fix wix display not tracking issue

--- src/shared/environment-data-sources/wix.ts
+++ src/shared/environment-data-sources/wix.ts
@@ -4,59 +4,61 @@ import { TransactionCartItem } from "../types";
 
 const wixTrackerDataSource = () => {
   function registerListeners() {
-    window.wixDevelopersAnalytics.register("conversionListener", (e, p) => {
-      if (e === "AddToCart") {
-        observable.notify({
-          addToCartEvent: {
-            sku: (p.sku || "N/A").toString(),
-            name: (p.name || "N/A").toString(),
-            category: (p.category || "N/A").toString(),
-            unitPrice: parseFloat(p.price),
-            quantity: parseFloat(p.quantity),
-            currency: (p.currency || "USD").toString(),
-          },
-        });
-      }
+    window.addEventListener('load', () => {
+      window.wixDevelopersAnalytics.register("conversionListener", (e, p) => {
+        if (e === "AddToCart") {
+          observable.notify({
+            addToCartEvent: {
+              sku: (p.sku || "N/A").toString(),
+              name: (p.name || "N/A").toString(),
+              category: (p.category || "N/A").toString(),
+              unitPrice: parseFloat(p.price),
+              quantity: parseFloat(p.quantity),
+              currency: (p.currency || "USD").toString(),
+            },
+          });
+        }
 
-      if (e === "RemoveFromCart") {
-        observable.notify({
-          removeFromCartEvent: {
-            sku: (p.sku || "N/A").toString(),
-            name: (p.name || "N/A").toString(),
-            category: (p.category || "N/A").toString(),
-            unitPrice: parseFloat(p.price),
-            quantity: parseFloat(p.quantity),
-            currency: (p.currency || "USD").toString(),
-          },
-        });
-      }
+        if (e === "RemoveFromCart") {
+          observable.notify({
+            removeFromCartEvent: {
+              sku: (p.sku || "N/A").toString(),
+              name: (p.name || "N/A").toString(),
+              category: (p.category || "N/A").toString(),
+              unitPrice: parseFloat(p.price),
+              quantity: parseFloat(p.quantity),
+              currency: (p.currency || "USD").toString(),
+            },
+          });
+        }
 
-      if (e === "Purchase") {
-        observable.notify({
-          transactionEvent: {
-            id: (p.orderId || "N/A").toString(),
-            total: parseFloat(p.revenue || 0),
-            tax: parseFloat(p.tax || 0),
-            shipping: parseFloat(p.shipping || 0),
-            city: "N/A",
-            state: "N/A",
-            country: "N/A",
-            currency: (p.currency || "USD").toString(),
-            items: p.contents.map((product) => {
-              return {
-                orderId: (p.orderId || "N/A").toString(),
-                productId: (product.id || "N/A").toString(),
-                sku: (product.sku || "N/A").toString(),
-                name: (product.name || "N/A").toString(),
-                category: (product.category || "N/A").toString(),
-                unitPrice: parseFloat(product.price || 0),
-                quantity: parseFloat(product.quantity || 1),
-                currency: (p.currency || "USD").toString(),
-              } as TransactionCartItem;
-            }),
-          },
-        });
-      }
+        if (e === "Purchase") {
+          observable.notify({
+            transactionEvent: {
+              id: (p.orderId || "N/A").toString(),
+              total: parseFloat(p.revenue || 0),
+              tax: parseFloat(p.tax || 0),
+              shipping: parseFloat(p.shipping || 0),
+              city: "N/A",
+              state: "N/A",
+              country: "N/A",
+              currency: (p.currency || "USD").toString(),
+              items: p.contents.map((product) => {
+                return {
+                  orderId: (p.orderId || "N/A").toString(),
+                  productId: (product.id || "N/A").toString(),
+                  sku: (product.sku || "N/A").toString(),
+                  name: (product.name || "N/A").toString(),
+                  category: (product.category || "N/A").toString(),
+                  unitPrice: parseFloat(product.price || 0),
+                  quantity: parseFloat(product.quantity || 1),
+                  currency: (p.currency || "USD").toString(),
+                } as TransactionCartItem;
+              }),
+            },
+          });
+        }
+      });
     });
   }
 
