5d9ceff51c2ecb7511a69ea7ec20b1b4cde1eb4c
pacholoamit <pacholoamit.tech@gmail.com>
2025-02-22 03:32:39 +0800

    style: Update import statements in register-third-party-tags file

--- src/shared/snowplow/extensions/register-third-party-tags.ts
+++ src/shared/snowplow/extensions/register-third-party-tags.ts
@@ -1,9 +1,7 @@
-import { SnowplowTracker } from 'src/shared/snowplow/types';
-import {
-  RegisterThirdPartyTagsInput
-} from 'src/shared/types';
+import { SnowplowTracker } from "src/shared/snowplow/types";
+import { RegisterThirdPartyTagsInput } from "src/shared/types";
 
-let thirdPartyTags: RegisterThirdPartyTagsInput = {}
+let thirdPartyTags: RegisterThirdPartyTagsInput = {};
 
 const registerThirdPartyTags = (input: RegisterThirdPartyTagsInput) => {
   thirdPartyTags = input;
@@ -16,7 +14,7 @@ const replaceMacros = (tag: string, placeholders: Record<string, string>) =>
 
 const createElementForTag = (type: string, tagWithMacros: string) => {
   if (type === "script") {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = tagWithMacros;
     script.async = true;
     document.body.appendChild(script);
@@ -41,61 +39,61 @@ const withRegisterThirdPartyTagsExtension = (snowplow: SnowplowTracker) => {
   snowplow.ecommerce.trackTransaction = (input) => {
     trackTransaction(input);
     processTags(thirdPartyTags.onTransaction, {
-      "{transaction_id}": input.id,
-      "{transaction_total}": input.total.toString(),
-      "{transaction_tax}": input.tax.toString(),
-      "{transaction_shipping}": input.shipping.toString(),
-      "{transaction_city}": input.city,
-      "{transaction_state}": input.state,
-      "{transaction_country}": input.country,
-      "{transaction_currency}": input.currency,
-      "{transaction_userId}": input.userId,
+      "{transaction_id}": input?.id?.toString() || "",
+      "{transaction_total}": input?.total?.toString() || "0",
+      "{transaction_tax}": input?.tax?.toString() || "0",
+      "{transaction_shipping}": input?.shipping?.toString() || "0",
+      "{transaction_city}": input?.city || "",
+      "{transaction_state}": input?.state || "",
+      "{transaction_country}": input?.country || "",
+      "{transaction_currency}": input?.currency || "",
+      "{transaction_userId}": input?.userId?.toString() || "",
     });
   };
 
   snowplow.ecommerce.trackAddToCart = (input) => {
     trackAddToCart(input);
     processTags(thirdPartyTags.onAddToCart, {
-      "{cart_id}": input.sku,
-      "{cart_name}": input.name,
-      "{cart_category}": input.category,
-      "{cart_unitPrice}": input.unitPrice.toString(),
-      "{cart_quantity}": input.quantity.toString(),
-      "{cart_currency}": input.currency,
-      "{cart_userId}": input.userId,
+      "{cart_id}": input?.sku || "",
+      "{cart_name}": input?.name || "",
+      "{cart_category}": input?.category || "",
+      "{cart_unitPrice}": input?.unitPrice?.toString() || "0",
+      "{cart_quantity}": input?.quantity?.toString() || "0",
+      "{cart_currency}": input?.currency || "",
+      "{cart_userId}": input?.userId?.toString() || "",
     });
   };
 
   snowplow.ecommerce.trackRemoveFromCart = (input) => {
     trackRemoveFromCart(input);
     processTags(thirdPartyTags.onRemoveFromCart, {
-      "{cart_id}": input.sku,
-      "{cart_name}": input.name,
-      "{cart_category}": input.category,
-      "{cart_unitPrice}": input.unitPrice.toString(),
-      "{cart_quantity}": input.quantity.toString(),
-      "{cart_currency}": input.currency,
-      "{cart_userId}": input.userId,
+      "{cart_id}": input?.sku || "",
+      "{cart_name}": input?.name || "",
+      "{cart_category}": input?.category || "",
+      "{cart_unitPrice}": input?.unitPrice?.toString() || "0",
+      "{cart_quantity}": input?.quantity?.toString() || "0",
+      "{cart_currency}": input?.currency || "",
+      "{cart_userId}": input?.userId?.toString() || "",
     });
   };
 
   snowplow.trackSignup = (input) => {
     trackSignup(input);
     processTags(thirdPartyTags.onSignup, {
-      "{signup_firstName}": input.firstName,
-      "{signup_lastName}": input.lastName,
-      "{signup_uuid}": input.uuid,
-      "{signup_email}": input.emailAddress,
-      "{signup_hashedEmailAddress}": input.hashedEmailAddress,
-      "{signup_address}": input.address,
-      "{signup_city}": input.city,
-      "{signup_state}": input.state,
-      "{signup_phone}": input.phoneNumber,
-      "{signup_advertiser}": input.advertiser,
+      "{signup_firstName}": input?.firstName || "",
+      "{signup_lastName}": input?.lastName || "",
+      "{signup_uuid}": input?.uuid || "",
+      "{signup_email}": input?.emailAddress || "",
+      "{signup_hashedEmailAddress}": input?.hashedEmailAddress || "",
+      "{signup_address}": input?.address || "",
+      "{signup_city}": input?.city || "",
+      "{signup_state}": input?.state || "",
+      "{signup_phone}": input?.phoneNumber || "",
+      "{signup_advertiser}": input?.advertiser || "",
     });
-  }
+  };
 
   return snowplow;
 };
 
-export default withRegisterThirdPartyTagsExtension
+export default withRegisterThirdPartyTagsExtension;
