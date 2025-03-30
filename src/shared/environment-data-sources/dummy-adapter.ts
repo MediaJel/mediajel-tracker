43b9eeb8ffde39a62458908e8d97571c3746f7af
SeanRizarre <sean@mediajel.com>
2025-03-31 04:00:55 +0800

    test:simulate triggering of 2nd use case on dummy adapter

--- src/shared/environment-data-sources/dummy-adapter.ts
+++ src/shared/environment-data-sources/dummy-adapter.ts
@@ -4,6 +4,7 @@ import dummyData from "../utils/dummyData";
 import { TransactionCartItem } from "../types";
 
 const dummyDataSource = () => {
+    const data1 = dummyData();
     const rawData = dummyData();
     console.log("Data Value:", JSON.stringify(rawData[0], null, 2));
     console.log("Items Value:", rawData[0].items[0]);
@@ -11,30 +12,32 @@ const dummyDataSource = () => {
     const items = data.items;
 
     const trackData = () => {
-        observable.notify({
-            transactionEvent: {
-              id: data.id,
-              total: data.total,
-              tax: data.tax,
-              shipping: data.shipping,
-              city: data.city,
-              state: data.state,
-              country: data.country,
-              currency: data.currency,
-              items: items.map((item) => {
-                const { sku, name, category, unitPrice, quantity } = item;
-                return {
-                  orderId: data.id,
-                  sku: sku,
-                  name: name,
-                  category: category,
-                  unitPrice: unitPrice,
-                  quantity: quantity,
+        if (data.cart === 1) {
+            observable.notify({
+                transactionEvent: {
+                  id: data.id,
+                  total: data.total,
+                  tax: data.tax,
+                  shipping: data.shipping,
+                  city: data.city,
+                  state: data.state,
+                  country: data.country,
                   currency: data.currency,
-                } as TransactionCartItem;
-              }) || [],
-            },
-        });
+                  items: items.map((item) => {
+                    const { sku, name, category, unitPrice, quantity } = item;
+                    return {
+                      orderId: data.id,
+                      sku: sku,
+                      name: name,
+                      category: category,
+                      unitPrice: unitPrice,
+                      quantity: quantity,
+                      currency: data.currency,
+                    } as TransactionCartItem;
+                  }),
+                },
+            });
+        }
     }
 
     isTrackerLoaded(trackData);
--- src/shared/utils/dummyData.ts
+++ src/shared/utils/dummyData.ts
@@ -1,5 +1,6 @@
 const dummyData = () => {
     const dummyData1 = [{
+        cart: 1,
         id: "1234",
         total: 100,
         tax: 10,
@@ -20,6 +21,7 @@ const dummyData = () => {
     }];
 
     const dummyData2 = [{
+        cart: 2,
         id: "5678",
         total: 50,
         tax: 5,
@@ -39,9 +41,11 @@ const dummyData = () => {
         ]
     }];
 
-    const randomizer = Math.random() < 0.5 ? dummyData1 : dummyData2;
-    console.log("Value of Randomizer: ", JSON.stringify(randomizer, null, 2));
-    return randomizer;
+    // const randomizer = Math.random() < 0.5 ? dummyData1 : dummyData2;
+    // const dummyDataValue = { randomizer, dummyData1, dummyData2 };
+    const randomizer = dummyData2;
+    const dummyDataValue = randomizer;
+    return dummyDataValue;
 }
 
 export default dummyData;
