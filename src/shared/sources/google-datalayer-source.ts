c7a461071ad6b55d7cd22a2b748b94eb8ce9742a
pacholoamit <pacholoamit.tech@gmail.com>
2025-04-02 03:03:37 +0800

    refactor: Update variable names for clarity

--- src/shared/sources/google-datalayer-source.ts
+++ src/shared/sources/google-datalayer-source.ts
@@ -1,16 +1,12 @@
-export const datalayerSource = (callback: (data: any) => void, layer: any = window.dataLayer || []): void => {
+export const datalayerSource = (callback: (data: any) => void, dataLayer: any = window.dataLayer || []): void => {
   // dataLayer is optional second param to handle dispense cart;
-  const originalPush = layer.push;
-
-  // Process any existing items in the dataLayer
-  if (layer && layer.length > 0) {
-    layer.forEach((item: any) => {
-      callback(item);
-    });
+  if (!dataLayer || typeof dataLayer.push !== 'function') {
+    console.error('dataLayer.push is not a function');
+    return;
   }
-
-  layer.push = (...args: any): void => {
+  const originalPush = dataLayer.push.bind(dataLayer);;
+  dataLayer.push = (...args: any): void => {
     originalPush(...args);
-    callback(layer.slice(-1)[0]); // Gets the newest array member of dataLayer
+    callback(dataLayer.slice(-1)[0]); // Gets the newest array member of dataLayer
   };
 };
