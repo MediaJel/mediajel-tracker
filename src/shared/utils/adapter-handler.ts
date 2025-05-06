f0168a05c82f06f73d855698657fbd40c312c884
pacholoamit <pacholoamit.tech@gmail.com>
2025-05-07 06:33:36 +0800

    remove isUnique

--- src/shared/utils/adapter-handler.ts
+++ src/shared/utils/adapter-handler.ts
@@ -5,69 +5,65 @@ import { SnowplowTracker } from "../snowplow/types";
 type HandlerFunction = () => void;
 
 interface AdapterHandler {
-    add: (name: string, fn: HandlerFunction) => void;
-    execute: () => void;
+  add: (name: string, fn: HandlerFunction) => void;
+  execute: () => void;
 }
 
 export const createAdapterHandler = (snowplow: SnowplowTracker) => {
-    const snowplowContext = snowplow.context.appId;
-    const storageKey = `${snowplowContext}_transaction`;
-    let successLogged = false;
-    let counter = 0;
-    const fns: Array<{ name: string; fn: HandlerFunction }> = [];
+  const snowplowContext = snowplow.context.appId;
+  const storageKey = `${snowplowContext}_transaction`;
+  let successLogged = false;
+  let counter = 0;
+  const fns: Array<{ name: string; fn: HandlerFunction }> = [];
 
-    function isUnique (name: string) {
-        return fns.findIndex((fn) => fn.name === name) === -1;
+  function isUnique(name: string) {
+    return fns.findIndex((fn) => fn.name === name) === -1;
+  }
+
+  const checkTransaction = () => {
+    try {
+      return !!localStorage.getItem(storageKey);
+    } catch (error) {
+      logger.error("Error checking transaction");
+      return false;
     }
+  };
 
-    const checkTransaction = () => {
+  return {
+    add: (name: string, fn: HandlerFunction) => {
+      fns.push({ name, fn });
+    },
+    execute: () => {
+      if (counter === 1) return;
+      if (successLogged || checkTransaction()) return;
+
+      for (const { name, fn } of fns) {
+        let success = false;
+        observable.subscribe((event) => {
+          if (event.transactionEvent) success = true;
+        });
+
         try {
-            return !!localStorage.getItem(storageKey);
+          logger.info(`Attempting transaction with ${name}`);
+          fn();
+
+          if (success) {
+            successLogged = true;
+            logger.info(`Transaction successful with ${name}`);
+            return;
+          }
         } catch (error) {
-            logger.error('Error checking transaction');
-            return false;
+          logger.error(`Transaction Failed with ${name}`, error);
         }
-    }
+      }
+      counter++;
 
-    return {
-        add: (name: string, fn: HandlerFunction) => {
-            if (!isUnique(name)) {
-                return;
-            }
-            fns.push({ name, fn });
-        },
-        execute: () => {
-            if (counter === 1) return;
-            if (successLogged || checkTransaction()) return;
-
-            for (const { name, fn } of fns) {
-                let success = false;
-                observable.subscribe((event) => {
-                    if (event.transactionEvent) success = true
-                });
-
-                try {
-                    logger.info(`Attempting transaction with ${name}`);
-                    fn();
-
-                    if (success) {
-                        successLogged = true;
-                        logger.info(`Transaction successful with ${name}`);
-                        return;
-                    }
-                } catch (error) {
-                    logger.error(`Transaction Failed with ${name}`, error);
-                }
-            }
-            counter++;
-
-            if (!successLogged) {
-                logger.error('All transaction attempts failed for the adapter.');
-            }
-        }
-    } as AdapterHandler;
-} 
-
+      if (!successLogged) {
+        logger.error("All transaction attempts failed for the adapter.");
+      }
+    },
+  } as AdapterHandler;
+};
 
 export const multiAdapterHandler = (() => {
   let instance: ReturnType<typeof createAdapterHandler> | null = null;
@@ -78,4 +74,3 @@ export const multiAdapterHandler = (() => {
     return instance;
   };
 })();
-
