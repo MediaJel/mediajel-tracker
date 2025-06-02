dc214e51c5eff90e4827b1aad2079146d70a4107
JC Dimazana <jc@mediajel.com>
2025-06-03 04:56:17 +0800

    fix spageti

--- src/shared/environment-data-sources/carrot.ts
+++ src/shared/environment-data-sources/carrot.ts
@@ -5,53 +5,58 @@ import { fetchSource } from "../sources/fetch-source";
 import { multiAdapterHandler } from "../utils/adapter-handler";
 import { SnowplowTracker } from "../snowplow/types";
 
+function x (responseBody) {
+    if (!window.location.href.includes('/store/activity')) return false;
+    if (!responseBody) return false;
+return true;
+}
+
 const carrotDataSource = (snowplow: SnowplowTracker) => {
+
   const handler = multiAdapterHandler(snowplow);
 
   handler.add("Fetch Response Source", () => {
-        fetchSource(
+    if (x) 
+        return
+        {fetchSource(
         (request) => { },
         (response, responseBody) => {
-            if (window.location.href.includes('/store/activity')) {
-                try {
-                    if (responseBody) {
-                        responseBody.orders.map((order) => {
-                            if (order.details.status === 'open') {
-                                const data = order;
-                                try {
-                                    isTrackerLoaded(() => {
-                                    observable.notify({
-                                        transactionEvent: {
-                                            id: data.details.id || "N/A",
-                                            total: parseFloat(data.details.total || 0),
-                                            tax: parseFloat(data.details.tax || 0),
-                                            shipping: 0,
-                                            city: "N/A",
-                                            state: "N/A",
-                                            country: "USA",
+            try {
+                responseBody.orders.map((order) => {
+                    if (order.details.status === 'open') {
+                        const data = order;
+                        try {
+                            isTrackerLoaded(() => {
+                            observable.notify({
+                                transactionEvent: {
+                                    id: data.details.id || "N/A",
+                                    total: parseFloat(data.details.total || 0),
+                                    tax: parseFloat(data.details.tax || 0),
+                                    shipping: 0,
+                                    city: "N/A",
+                                    state: "N/A",
+                                    country: "USA",
+                                    currency: "USD",
+                                    items: data.items.map((item) => {
+                                        return {
+                                            orderId: data.details.id || "N/A",
+                                            sku: (item.product.id).toString() || "N/A",
+                                            name: (item.product.name || "N/A").toString(),
+                                            category: (item.product.categoryName || "N/A").toString(),
+                                            unitPrice: parseFloat(item.price) || 0,
+                                            quantity: parseInt(item.unitQty) || 1,
                                             currency: "USD",
-                                            items: data.items.map((item) => {
-                                                return {
-                                                    orderId: data.details.id || "N/A",
-                                                    sku: (item.product.id).toString() || "N/A",
-                                                    name: (item.product.name || "N/A").toString(),
-                                                    category: (item.product.categoryName || "N/A").toString(),
-                                                    unitPrice: parseFloat(item.price) || 0,
-                                                    quantity: parseInt(item.unitQty) || 1,
-                                                    currency: "USD",
-                                                } as TransactionCartItem;
-                                            }) || [],
-                                        },
-                                    });
-                                });
-                                } catch (error) {}
-                            } 
-                        })
-                    }
-                } catch (error) {}
-            }
+                                        } as TransactionCartItem;
+                                    }) || [],
+                                },
+                            });
+                        });
+                        } catch (error) {}
+                    } 
+                })
+            } catch (error) {}
         }
-    );
+    );}
   });
 };
 
