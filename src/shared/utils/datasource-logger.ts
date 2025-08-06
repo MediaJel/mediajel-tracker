86eec482b5357cada9634f5cdeead9e791a87ce9
pacholoamit <pacholoamit.tech@gmail.com>
2025-08-07 05:11:22 +0800

    Revert "Merge pull request #762 from MediaJel/staging"
    This reverts commit 8849f1fddb5669bda3aa6ec12e3d2cc8897e31d5, reversing
    changes made to 6ad318750b098532f673cb6fbd13c718f76a97a7.

--- src/shared/utils/datasource-logger.ts
+++ src/shared/utils/datasource-logger.ts
@@ -1,44 +1,43 @@
 import { datalayerSource } from "../sources/google-datalayer-source";
 import { xhrRequestSource, formatXhrPayload } from "../sources/xhr-request-source";
-import { xhrResponseSource } from "../sources/xhr-response-source";
+import {  xhrResponseSource } from "../sources/xhr-response-source";
 import { fetchSource } from "../sources/fetch-source";
 import { createLogger } from "../logger";
 
 export const datasourceLogger = (): void => {
-  const logger = createLogger("DataSourceLogger");
+    const logger = createLogger("DataSourceLogger");
 
-  datalayerSource((data) => {
-    console.log("DataLayer Source Data:", data);
-  });
+    datalayerSource((data) => {
+        logger.info("DataLayer Source Data:", data);
+    });
 
-  xhrResponseSource((xhr) => {
-    try {
-      if (xhr.responseURL == "https://otel.highlight.io/v1/metrics") return;
-      console.log("xhr Response Source Data: ", xhr);
-    } catch (e) {}
-  });
+    xhrResponseSource((xhr) => {
+        try {
+            logger.info('xhr Response Source Data: ', xhr);
+        } catch  (e) {}
+    });
 
-  xhrRequestSource((xhr) => {
-    try {
-      const sanitizedData = formatXhrPayload(xhr);
-      console.log("xhr Request Source Data: ", sanitizedData);
-    } catch (e) {}
-  });
+    xhrRequestSource((xhr) => {
+        try {
+            const sanitizedData = formatXhrPayload(xhr);
+            logger.info('xhr Request Source Data: ', sanitizedData);
+        } catch (e) {}
+    });
 
-  window.addEventListener("message", (event) => console.log("post message data", event), false);
+    window.addEventListener("Event Listener Message1", (event) => logger.info('post message data',event), false);
 
-  const postMessageSource = function (callback) {
-    window.addEventListener("message", callback, false);
-  };
+    const postMessageSource = function(callback) {
+        window.addEventListener("Post Message Data", callback, false);
+      };
+    
+    postMessageSource((event) => logger.info('Post Message Data: ', event));
 
-  postMessageSource((event) => console.log("Post Message Data: ", event));
-
-  fetchSource(
-    (request) => {},
-    (response, responseBody) => {
-      try {
-        console.log("fetch response: ", responseBody);
-      } catch (e) {}
-    },
-  );
-};
+    fetchSource(
+        (request) => {},
+        (response, responseBody) => {
+            try {
+                logger.info('fetch response: ', responseBody);
+            } catch (e) {}
+        }
+      );
+}
