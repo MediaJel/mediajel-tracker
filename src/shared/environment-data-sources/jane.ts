a04ef0c1433291116c9c616e6879dcd6a53fd3dd
Jon <jon@mediajel.com>
2026-01-09 10:24:34 +0800

    hotfix bring back reverted features

--- src/shared/environment-data-sources/jane.ts
+++ src/shared/environment-data-sources/jane.ts
@@ -58,6 +58,7 @@ const janeDataSource = (snowplow: SnowplowTracker) => {
             deliveryAddress = {},
             salesTax,
             storeTax,
+            discountTotal,
           } = payload.properties;
   
           observable.notify({
@@ -70,6 +71,8 @@ const janeDataSource = (snowplow: SnowplowTracker) => {
               city: (deliveryAddress?.city || "N/A").toString(),
               state: (deliveryAddress?.state_code || "N/A").toString(),
               country: (deliveryAddress?.country_code || "N/A").toString(),
+              discount: parseFloat(discountTotal || 0),
+              couponCode: "N/A",
               currency: "USD",
               items: products.map((product) => {
                 const { product_id, name, category, unit_price, count } = product;
